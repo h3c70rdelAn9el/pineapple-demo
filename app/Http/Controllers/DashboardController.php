22775d5cb0180126d859c313b40e9e24da39cf81
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-11-07 17:17:56 -0800

    add admin to user table & make route

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    //
+    public function index()
+    {
+        $user = auth()->user();
+
+        // $therapysessions = TherapySession::where('user_id', $user->id)->get();
+        // $therapists = User::where('admin', 0)->get();
+
+        if ($user->admin)
+            // return view('dashboard_admin', ['therapists' => $therapists, 'user' => $user]);
+            return view('dashboard_admin');
+        else
+            // return view('dashboard', ['therapysessions' => $therapysessions, 'user' => $user]);
+            return view('dashboard');
+    }
+}
