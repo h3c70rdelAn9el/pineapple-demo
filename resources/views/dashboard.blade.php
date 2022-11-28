86962b0b6f7472e4eb1c5f595b435e438fc8fdfe
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-11-27 22:04:08 -0800

    style 💄 the dashboard page a little

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,49 +7,75 @@
 
     <div class="py-8">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="bg-white shadow-xl sm:rounded-lg">
+            <div class="p-2 capitalize bg-white shadow-xl sm:rounded-lg">
                 {{-- <x-jet-welcome /> --}}
                 <p class="text-xl">welcome<span class="font-bold"> {{ $user->name }}</span></p>
             </div>
 
-                <div class="flex flex-row">
-                    @foreach ($patients as $patient)
-                    <div class="flex flex-row w-40 p-2 m-2 transition-all duration-200 ease-in rounded-md shadow-md hover:scale-105 hover:shadow-lg">
-                        <a href="{{ route('patient', ['patient' => $patient, 'patient_id' => $patient->id]) }}" class="flex flex-row">
-                            <p class="flex flex-row">{{ $patient->first }} {{ $patient->last }}</p>
-                        </a>
+            <div class="w-5/6 p-2 mx-auto mt-2 rounded-md shadow-md bg-blue-50 lg:w-1/2 shadow-blue-100">
+                <p class="text-lg font-bold">Add New Patient:</p>
+                <form action="{{ route('patient.store') }}" class="p-4">
+                    @csrf
+                    <div>
+                        <label for="first">first</label>
+                        <input type="text"
+                            id="first"
+                            name="first"
+                            class="form-input"
+                            >
                     </div>
-                    @endforeach
-                </div>
+                    <div>
+                        <label for="last">last</label>
+                        <input type="text"
+                            id="last"
+                            name="last"
+                            class="form-input"
+                            >
+                    </div>
+                    <div>
+                        <label for="email">email</label>
+                        <input type="text"
+                            id="email"
+                            name="email"
+                            class="form-input"
+                            >
+                    </div>
+                    <div>
+                        <label for="phone">phone</label>
+                        <input type="text"
+                            id="phone"
+                            name="phone"
+                            class="form-input"
+                            >
+                    </div>
+                    <div>
+                        <label for="insurance">insurance</label>
+                        <input type="text"
+                            id="insurance"
+                            name="insurance"
+                            class="form-input">
+                    </div>
+                    <div class="mt-2">
+                        <button type="submit" class="px-2 py-1 duration-200 bg-blue-300 rounded-md hover:scale-110">
+                            Add
+                        </button>
+                    </div>
 
-            <form
-                action="{{ route('patient.store') }}">
-                @csrf
-                <div>
-                    <label for="first">first</label>
-                    <input type="text" id="first" name="first">
-                </div>
-                <div>
-                    <label for="last">last</label>
-                    <input type="text" id="last" name="last">
-                </div>
-                <div>
-                    <label for="email">email</label>
-                    <input type="text" id="email" name="email">
-                </div>
-                <div>
-                    <label for="phone">phone</label>
-                    <input type="text" id="phone" name="phone">
-                </div>
-                <div>
-                    <label for="insurance">insurance</label>
-                    <input type="text" id="insurance" name="insurance">
-                </div>
-                <button type="submit">
-                    Add
-                </button>
+                </form>
+            </div>
 
-            </form>
+            <div class="flex flex-row flex-wrap justify-center">
+                @foreach ($patients as $patient)
+                    <a href="{{ route('patient', ['patient' => $patient, 'patient_id' => $patient->id]) }}"
+                        class="flex flex-row">
+                        <div
+                            class="flex flex-row w-40 p-2 m-2 text-center transition-all duration-200 ease-in rounded-md shadow-md shadow-blue-100 bg-blue-50 hover:scale-105 hover:shadow-blue-200 hover:shadow-lg">
+                            <p class="text-center capitalize ">{{ $patient->first }} {{ $patient->last }}</p>
+                        </div>
+                    </a>
+                @endforeach
+            </div>
+
         </div>
     </div>
 </x-app-layout>
