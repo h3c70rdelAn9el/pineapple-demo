4cc03caab2cda7703f9bcede84393ef95d46eb95
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-01 12:58:09 -0800

    add patient info into therapist page

--- app/Http/Controllers/TherapistsController.php
+++ app/Http/Controllers/TherapistsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Patient;
 use Illuminate\Http\Request;
 use App\Models\TherapySession;
 
@@ -19,8 +20,10 @@ class TherapistsController extends Controller
     {
         $user = auth()->user();
         $therapist = User::find($id);
+        // TODO:  RETURN PROPER PATIENTS FOR THERAPIST
+        $patients = Patient::all();
         $therapySessions = TherapySession::where('');
 
-        return view('therapist.show', ['user' => $user, 'therapist' => $therapist, 'therapySessions' => $therapySessions]);
+        return view('therapist.show', ['user' => $user, 'therapist' => $therapist, 'therapySessions' => $therapySessions, 'patients' => $patients]);
     }
 }
