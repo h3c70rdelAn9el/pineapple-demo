b2267c7783c6a07c6ad8ecdf7d9e864327079a4d
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-07 15:16:21 -0800

    add credentials upload on profile page

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -22,6 +22,8 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
             'photo' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
+            'license' => ['required', 'string', 'max:255'],
+            'expires_at' => ['required', 'date' ]
         ])->validateWithBag('updateProfileInformation');
 
         if (isset($input['photo'])) {
@@ -35,6 +37,8 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             $user->forceFill([
                 'name' => $input['name'],
                 'email' => $input['email'],
+                'license' => $input['license'],
+                'expires_at' => $input['expires_at']
             ])->save();
         }
     }
