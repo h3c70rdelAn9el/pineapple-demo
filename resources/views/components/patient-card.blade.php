b64511f03a7e15c50753e3e0cfb951764eec76aa
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-07 14:38:14 -0800

    style dashboard page

---
+++ resources/views/components/patient-card.blade.php
@@ -0,0 +1,7 @@
+         <a href="{{ route('patient', ['patient' => $patient, 'patient_id' => $patient->id]) }}"
+                    class="flex flex-row">
+                    <div
+                        class="flex flex-row w-40 p-2 m-2 text-center transition-all duration-200 ease-in rounded-md shadow-md bg-blue-50 shadow-blue-100 hover:scale-105 hover:shadow-lg hover:shadow-blue-200">
+                        <p class="text-center capitalize">{{ $patient->first }} {{ $patient->last }}</p>
+                    </div>
+                </a>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,78 +5,44 @@
         </h2>
     </x-slot>
 
-    <div class="py-8">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="p-2 capitalize bg-white shadow-lg shadow-blue-100 sm:rounded-lg">
-                {{-- <x-jet-welcome /> --}}
-                <p class="text-xl">welcome<span class="font-bold"> {{ $user->name }}</span></p>
+    <div class="w-11/12 mx-auto border border-blue-500 shadow-lg max-w-7xl rounded-xl shadow-blue-100 sm:px-6 lg:px-8">
+        <section class="flex flex-row items-center w-full max-w-5xl mx-auto justify-evenly md:flex-row md:justify-between">
+            <div class="p-4 text-center capitalize bg-white shadow-sm sm:rounded-lg">
+                <p class="text-lg md:text-xl">Welcome<span class="font-bold"> {{ $user->name }}</span></p>
             </div>
 
-            <div class="w-5/6 p-2 mx-auto mt-2 rounded-md shadow-md bg-blue-50 lg:w-1/2 shadow-blue-100">
-                <p class="text-lg font-bold">Add New Patient:</p>
-                <form action="{{ route('patient.store') }}" class="p-4">
-                    @csrf
-                    <div>
-                        <label for="first">first</label>
-                        <input type="text"
-                            id="first"
-                            name="first"
-                            class="form-input"
-                            >
-                    </div>
-                    <div>
-                        <label for="last">last</label>
-                        <input type="text"
-                            id="last"
-                            name="last"
-                            class="form-input"
-                            >
-                    </div>
-                    <div>
-                        <label for="email">email</label>
-                        <input type="text"
-                            id="email"
-                            name="email"
-                            class="form-input"
-                            >
-                    </div>
-                    <div>
-                        <label for="phone">phone</label>
-                        <input type="text"
-                            id="phone"
-                            name="phone"
-                            class="form-input"
-                            >
-                    </div>
-                    <div>
-                        <label for="insurance">insurance</label>
-                        <input type="text"
-                            id="insurance"
-                            name="insurance"
-                            class="form-input">
-                    </div>
-                    <div class="mt-2">
-                        <button type="submit" class="px-2 py-1 duration-200 bg-blue-300 rounded-md hover:scale-110">
-                            Add
-                        </button>
-                    </div>
-
-                </form>
+            <x-clock></x-clock>
+            <div class="hidden p-4 text-center bg-white rounded-lg shadow-sm md:block">
+                Total Patients: {{ $patients->count() }}
             </div>
-
-            <h3 class="mt-3 text-lg font-bold text-center">Patients:</h3>
-            <div class="flex flex-row flex-wrap justify-center">
-                @foreach ($patients as $patient)
-                    <a href="{{ route('patient', ['patient' => $patient, 'patient_id' => $patient->id]) }}"
-                        class="flex flex-row">
-                        <div
-                            class="flex flex-row w-40 p-2 m-2 text-center transition-all duration-200 ease-in rounded-md shadow-md shadow-blue-100 bg-blue-50 hover:scale-105 hover:shadow-blue-200 hover:shadow-lg">
-                            <p class="text-center capitalize ">{{ $patient->first }} {{ $patient->last }}</p>
-                        </div>
-                    </a>
-                @endforeach
+            <div>
+        </section>
+        <div class="flex">
+            <div class="p-4 mx-auto text-center bg-white shadow-lg rounded-xl md:hidden md:text-lg">
+                Total Patients: {{ $patients->count() }}
             </div>
-
         </div>
+
+
+        <h3 class="mt-3 text-lg font-bold text-center">Patients:</h3>
+        <div class="flex flex-row flex-wrap justify-center">
+            @foreach ($patients as $patient)
+                {{-- <a href="{{ route('patient', ['patient' => $patient, 'patient_id' => $patient->id]) }}"
+                    class="flex flex-row">
+                    <div
+                        class="flex flex-row w-40 p-2 m-2 text-center transition-all duration-200 ease-in rounded-md shadow-md bg-blue-50 shadow-blue-100 hover:scale-105 hover:shadow-lg hover:shadow-blue-200">
+                        <p class="text-center capitalize">{{ $patient->first }} {{ $patient->last }}</p>
+                    </div>
+                </a> --}}
+                @include('components.patient-card')
+            @endforeach
+        </div>
+
+
+        <div class="w-5/6 p-2 mx-auto mt-2 rounded-md shadow-md bg-blue-50 shadow-blue-100 lg:w-1/2">
+            <p class="text-lg font-bold">Add New Patient:</p>
+            <x-dashboard-form></x-dashboard-form>
+        </div>
+
     </div>
 </x-app-layout>
