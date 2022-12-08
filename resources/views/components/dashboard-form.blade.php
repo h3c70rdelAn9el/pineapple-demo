62e995fd87e1caa85f6109a9b982bd59a25d65f1
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-07 17:54:52 -0800

    style add patient form & make compoenents for inputs

--- resources/views/components/dashboard-form.blade.php
+++ resources/views/components/dashboard-form.blade.php
@@ -1,45 +1,58 @@
 <form action="{{ route('patient.store') }}"
-                class="p-4">
-                @csrf
-                <div>
-                    <label for="first">First</label>
-                    <input type="text"
-                        id="first"
-                        name="first"
-                        class="form-input">
-                </div>
-                <div>
-                    <label for="last">Last</label>
-                    <input type="text"
-                        id="last"
-                        name="last"
-                        class="form-input">
-                </div>
-                <div>
-                    <label for="email">Email</label>
-                    <input type="text"
-                        id="email"
-                        name="email"
-                        class="form-input">
-                </div>
-                <div>
-                    <label for="phone">Phone</label>
-                    <input type="text"
-                        id="phone"
-                        name="phone"
-                        class="form-input">
-                </div>
-                <div>
-                    <label for="insurance">Insurance</label>
-                    <input type="text"
-                        id="insurance"
-                        name="insurance"
-                        class="form-input">
-                </div>
-                <div class="mt-2">
-                    <button type="submit"
-                        class="px-2 py-1 duration-200 bg-blue-300 rounded-md hover:scale-110">
-                        Add
-                    </button>
-                </div>
-            </form>
+    class="p-4 mt-2">
+    @csrf
+    <x-form_input_div>
+        <x-form_input id="first"
+            type="text"
+            name="first"
+            class=""
+            placeholder="First"
+            wire:model.defer="state.expires_at" />
+        <x-form_label for="first">
+            First Name
+        </x-form_label>
+    </x-form_input_div>
+
+    <x-form_input_div>
+        <x-form_input id="last"
+            type="text"
+            name="last"
+            placeholder="Last" />
+        <x-form_label for="last">
+            Last Name
+        </x-form_label>
+    </x-form_input_div>
+    <x-form_input_div>
+        <x-form_input id="email"
+            type="email"
+            name="email"
+            placeholder="Email" />
+        <x-form_label for="email">
+            Email
+        </x-form_label>
+    </x-form_input_div>
+    <x-form_input_div>
+        <x-form_input id="phone"
+            type="text"
+            name="phone"
+            placeholder="Phone" />
+        <x-form_label for="phone">
+            Phone
+        </x-form_label>
+    </x-form_input_div>
+    <x-form_input_div>
+        <x-form_input id="phone"
+            type="text"
+            name="insurance"
+            placeholder="Insurance" />
+        <x-form_label for="insurance">
+            Insurance
+        </x-form_label>
+    </x-form_input_div>
+    <div class="mt-2">
+        <button type="submit"
+            class="px-2 py-1 duration-200 bg-blue-300 rounded-md hover:scale-110">
+            Add
+        </button>
+    </div>
+</form>
---
+++ resources/views/components/form_label.blade.php
@@ -0,0 +1,5 @@
+@props(['value'])
+
+<label {{ $attributes->merge(['class' => 'absolute left-1 -top-3.5 text-xs text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-5 peer-focus:text-sm peer-focus:text-gray-600']) }}>
+    {{ $value ?? $slot }}
+</label>
