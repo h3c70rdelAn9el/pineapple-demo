5658a4898c003b4f8470ceb927221455caa34f6d
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-07 17:54:52 -0800

    style add patient form & make compoenents for inputs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,12 +6,14 @@
     </x-slot>
 
     <div class="w-11/12 mx-auto border border-blue-500 shadow-lg max-w-7xl rounded-xl shadow-blue-100 sm:px-6 lg:px-8">
-        <section class="flex flex-row items-center w-full max-w-5xl mx-auto justify-evenly md:flex-row md:justify-between">
+        <section class="flex flex-row items-center justify-around w-full max-w-5xl mx-auto md:flex-row">
             <div class="p-4 text-center capitalize bg-white shadow-sm sm:rounded-lg">
                 <p class="text-lg md:text-xl">Welcome<span class="font-bold"> {{ $user->name }}</span></p>
             </div>
 
-            <x-clock></x-clock>
+            <div>
+                <x-clock></x-clock>
+            </div>
             <div class="hidden p-4 text-center bg-white rounded-lg shadow-sm md:block">
                 Total Patients: {{ $patients->count() }}
             </div>
@@ -27,22 +29,14 @@
         <h3 class="mt-3 text-lg font-bold text-center">Patients:</h3>
         <div class="flex flex-row flex-wrap justify-center">
             @foreach ($patients as $patient)
-                {{-- <a href="{{ route('patient', ['patient' => $patient, 'patient_id' => $patient->id]) }}"
-                    class="flex flex-row">
-                    <div
-                        class="flex flex-row w-40 p-2 m-2 text-center transition-all duration-200 ease-in rounded-md shadow-md bg-blue-50 shadow-blue-100 hover:scale-105 hover:shadow-lg hover:shadow-blue-200">
-                        <p class="text-center capitalize">{{ $patient->first }} {{ $patient->last }}</p>
-                    </div>
-                </a> --}}
                 @include('components.patient-card')
             @endforeach
         </div>
 
 
-        <div class="w-5/6 p-2 mx-auto mt-2 rounded-md shadow-md bg-blue-50 shadow-blue-100 lg:w-1/2">
+        <div class="w-5/6 p-2 mx-auto mt-2 mb-4 rounded-md shadow-md bg-blue-50 shadow-blue-100 lg:w-1/2">
             <p class="text-lg font-bold">Add New Patient:</p>
             <x-dashboard-form></x-dashboard-form>
         </div>
-
     </div>
 </x-app-layout>
