f6b2da2a44b43890237762db1e2afb84e4b99133
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-09 18:53:14 -0800

    style the admin page

--- resources/views/dashboard_admin.blade.php
+++ resources/views/dashboard_admin.blade.php
@@ -1,41 +1,54 @@
 <x-app-layout>
-    <x-slot name="header">
+    {{-- <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('Admin Dashboard') }}
         </h2>
-    </x-slot>
- <div class="min-h-screen">
-        <div class="container p-2 capitalize bg-white shadow-lg shadow-blue-100 sm:rounded-lg">
-            {{-- <x-jet-welcome /> --}}
-            <p class="text-xl">welcome<span class="font-bold"> {{ $user->name }}</span></p>
-        </div>
-        <div class="w-2/3 h-full p-4 mx-auto mt-3 bg-white rounded-md">
-            <div class="flex flex-row flex-wrap mb-2 text-lg">
-                <p>Number of Therpists:</p>
-                <p class="ml-2">{{ $therapists->count() }}</p>
+    </x-slot> --}}
+    {{-- <div class="min-h-screen"> --}}
+    <div class="w-11/12 pb-4 mx-auto mt-20 bg-gray-100 border border-blue-500 shadow-lg max-w-7xl rounded-xl shadow-blue-100">
+        <section class="flex flex-row items-center justify-around w-full mx-auto text-white bg-blue-500 rounded-t-md md:flex-row">
+            <div class="p-4 text-center capitalize shadow-md sm:rounded-lg">
+                <p class="text-lg md:text-xl">Welcome<span class="font-bold"> {{ $user->name }}</span></p>
             </div>
-            <div class="grid grid-cols-4 gap-4 bg-white">
-                @forelse ($therapists as $therapist)
-                    <a href="{{ route('therapist.show', $therapist->id) }}"
-                        class="duration-200 hover:scale-105 group">
-                        <div
-                            class="w-40 text-center capitalize border rounded-md shadow-xl border-b-blue-200 bg-blue-50 shadow-blue-50">
-                            <p>{{ $therapist->name }}</p>
-                        </div>
-                    </a>
-                @empty
-                @endforelse
+
+            <div class="bg-blue-500">
+                <x-clock class="text-xl font-bold text-white bg-blue-500"></x-clock>
             </div>
-            <div>
-                <p>Patients</p>
-                <p>{{ $allpatients->count() }}</p>
-                <div class="grid grid-cols-4 gap-4 bg-white">
-                    @forelse ($allpatients as $patient)
-                        @include('components/patient-card')
+        </section>
+        <div class="flex flex-col w-full h-full max-w-6xl p-4 mx-auto mt-3 rounded-md md:flex-row">
+            <div class="p-2 m-2 bg-blue-100 rounded-md shadow-sm md:w-1/2">
+                <div class="flex flex-row flex-wrap justify-between mx-12 mb-2 text-lg border-b border-gray-100">
+                    <p class="font-bold ">Therpists:</p>
+                    <p class="ml-2">Total: <span class="font-bold">{{ $therapists->count() }}</span></p>
+                </div>
+                {{-- <div class="grid grid-cols-4 gap-4 bg-white"> --}}
+                <div class="flex flex-row flex-wrap justify-center mx-auto overflow-hidden">
+
+                    @forelse ($therapists as $therapist)
+                        @include('components/therapists-card')
                     @empty
                     @endforelse
                 </div>
             </div>
+
+            <div class="p-2 m-2 bg-blue-100 rounded-md shadow-sm md:w-1/2">
+                <div class="flex flex-wrap justify-between mx-12 mb-2 text-lg">
+                    <p class="font-bold">Patients:</p>
+                    <p class="ml-2">Total: <span class="font-bold">{{ $allpatients->count() }}</span></p>
+                </div>
+                {{-- <div class="grid grid-cols-4 gap-4 bg-white"> --}}
+                <div class="flex">
+                    <div class="flex flex-row flex-wrap justify-center mx-auto overflow-hidden">
+                        @forelse ($allpatients as $patient)
+                            @include('components/patient-card')
+                        @empty
+                        @endforelse
+                    </div>
+                </div>
+            </div>
         </div>
- </div>
+    </div>
+
+
+{{--  --}}
 </x-app-layout>
