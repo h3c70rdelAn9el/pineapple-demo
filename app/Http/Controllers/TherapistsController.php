7b88830c14c31039722caa429ae35897a29428a6
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-14 22:13:19 -0800

    clean up some comments

--- app/Http/Controllers/TherapistsController.php
+++ app/Http/Controllers/TherapistsController.php
@@ -17,29 +17,13 @@ class TherapistsController extends Controller
         return view('therapist.index')->with(['therapists' => $therapists]);
     }
 
-    // public function show(User $user, TherapySession $therapySession, $id, Client $client)
     public function show($id)
     {
         // $user = auth()->user($id);
         $therapist = User::find($id);
-
-        // $user_id = User::find($id);
-        // TODO:  RETURN PROPER PATIENTS FOR THERAPIST
-        // $clients = Client::all();
-
-        // client_id ->user->user_id
-        // $clients = Client::where('user_id');
         $clients = $therapist->clients()->get();
 
-        // $clients = Client::where('user_id')->get();
-
-        //   foreach ($clients as $client) {
-        //     $clients[] = ['id' => $client->id, 'chosen_name' => $client->chosen_name];
-        // }
         $therapySessions = TherapySession::where('');
-
-
-        // dd($clients);
 
         return view('therapist.show', ['therapist' => $therapist, 'therapySessions' => $therapySessions, 'clients' => $clients]);
     }
