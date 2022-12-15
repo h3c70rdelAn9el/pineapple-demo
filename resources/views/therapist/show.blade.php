d2fcee1aebf7399b55289165f1b451689637faa6
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-14 22:13:19 -0800

    clean up some comments

--- resources/views/therapist/show.blade.php
+++ resources/views/therapist/show.blade.php
@@ -4,20 +4,4 @@
         <p class="mr-2">Number of patients: </p>
         <p>{{ $clients->count() }}</p>
     </div>
-    {{-- <h1>{{ $user->name }}</h1> --}}
-    {{-- TODO:  PULL IN PROPER PATIENTS FOR THERAPISTS! --}}
-    {{-- <div class="grid grid-cols-2 gap-3 sm:grid-cols-3">
-        @forelse ($patients as $patient)
-            <a href="{{ route('patient', $patient->id) }}">
-                <div class="p-3 text-center rounded-md shadow-md shadow-blue-50 hover:shadow-xl">
-                    <p>{{ $patient->first }} {{ $patient->last }}</p>
-                </div>
-            </a>
-        @empty
-    </div>
-    No Patients to display
-    @endforelse --}}
-    <div>
-
-    </div>
 </x-app-layout>
