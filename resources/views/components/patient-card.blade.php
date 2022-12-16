edb5a3132e28d9d8d18b22eb6030517576e2106f
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-16 12:52:06 -0800

    add & style session-card & dashboard

--- resources/views/components/patient-card.blade.php
+++ resources/views/components/patient-card.blade.php
@@ -1,7 +1,8 @@
 <a href="{{ route('patient', $patient->id) }}"
     class="flex flex-row">
     <div
-        class="flex flex-row w-40 p-2 m-2 text-center transition-all duration-200 ease-in bg-blue-200 rounded-md shadow-md shadow-blue-100 hover:scale-105 hover:shadow-lg">
+        class="relative w-full p-2 m-2 duration-200 bg-blue-200 border border-blue-300 rounded-lg shadow-md lg:w-44 shadow-blue-100 hover:shadow-lg">
+
         <p class="text-center capitalize">{{ $patient->first }} {{ $patient->last }}</p>
     </div>
 </a>
