9e6682e5bc045b83f60c860ba52b2696cb9261fd
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-16 18:05:02 -0800

    display proper sessions on therapist page

--- resources/views/therapist/show.blade.php
+++ resources/views/therapist/show.blade.php
@@ -3,20 +3,21 @@
         <x-container-header :user="$user">
 
         </x-container-header>
-        <div class="grid items-center w-full h-full max-w-6xl grid-cols-3 p-4 mx-auto mt-3 rounded-md md:flex-row">
+        <div class="grid items-center w-full h-full max-w-6xl grid-cols-3 gap-3 p-4 mx-auto mt-3 rounded-md md:flex-row">
 
-            <div class="w-full text-center">
-                <h1 class="text-xl capitalize">Therapist: <span class="font-bold">{{ $therapist->name }}</span></h1>
+            <div class="w-full p-2 m-2 text-center bg-blue-100 rounded-lg">
+                <h1 class="capitalize">Therapist: <span class="text-lg font-bold">{{ $therapist->name }}</span></h1>
                 {{-- TODO:  add credentials --}}
             </div>
 
-            <div class="flex flex-row justify-center">
-                <p class="mr-2 text-center">Number of patients: </p>
+            <div class="flex flex-row justify-center w-full p-2 m-2 text-center bg-blue-100 rounded-lg">
+                <p class="mr-2 text-center">Clients: </p>
                 <p>{{ $clients->count() }}</p>
             </div>
             {{-- TODO:  ADD SESSIONS --}}
-            <div class="text-center">
-                sessions go here
+            <div class="flex flex-row w-full p-2 m-2 text-center bg-blue-100 rounded-lg">
+                <p class="text-center ">Sessions: </p>
+                <p>{{ $therapySessions->count() }}</p>
             </div>
         </div>
     </x-main-container>
