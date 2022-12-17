f94cbca84f2d8f63c2d6977e501e5acd16316ff3
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-16 16:12:25 -0800

    style more therapist

--- resources/views/therapist/show.blade.php
+++ resources/views/therapist/show.blade.php
@@ -5,17 +5,17 @@
         </x-container-header>
         <div class="grid items-center w-full h-full max-w-6xl grid-cols-3 p-4 mx-auto mt-3 rounded-md md:flex-row">
 
-            <div>
+            <div class="w-full text-center">
                 <h1 class="text-xl capitalize">Therapist: <span class="font-bold">{{ $therapist->name }}</span></h1>
                 {{-- TODO:  add credentials --}}
             </div>
 
-            <div class="flex flex-row">
-                <p class="mr-2">Number of patients: </p>
+            <div class="flex flex-row justify-center">
+                <p class="mr-2 text-center">Number of patients: </p>
                 <p>{{ $clients->count() }}</p>
             </div>
             {{-- TODO:  ADD SESSIONS --}}
-            <div>
+            <div class="text-center">
                 sessions go here
             </div>
         </div>
