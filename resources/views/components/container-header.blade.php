578dd27c1c78a0f981f0a5a68f0d2620629002ac
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-18 15:57:12 -0800

    refactor dashboard page

--- resources/views/components/container-header.blade.php
+++ resources/views/components/container-header.blade.php
@@ -1,10 +1,10 @@
 <section
     class="flex flex-row items-center justify-around w-full mx-auto text-white bg-blue-500 rounded-t-md md:flex-row">
-    <div class="p-4 text-center capitalize shadow-md sm:rounded-lg">
-        <p class="text-lg md:text-xl">Welcome: <span class="font-bold">{{ $user->name }}</span></p>
+    <div class="flex flex-col p-4 text-center capitalize md:flex-row text-md md:text-xl">
+        <p class="">Welcome:</p>
+        <p class="ml-1 font-bold md:ml-0">{{ $user->name }}</p>
     </div>
-    <div class="bg-blue-500 w-44">
-        <x-clock class="text-xl font-bold text-white bg-blue-500"></x-clock>
+    <div class="bg-blue-500 md:w-44 min-w-min">
+        <x-clock class="text-base font-bold text-white bg-blue-500"></x-clock>
     </div>
-
 </section>
