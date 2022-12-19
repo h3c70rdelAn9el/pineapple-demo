9783b22934f654b499a89289018bb78bda5ec65e
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-19 10:01:03 -0800

    clean up some comments

--- resources/views/components/client-form.blade.php
+++ resources/views/components/client-form.blade.php
@@ -2,7 +2,7 @@
     class="p-4 mt-2">
     @csrf
     <x-form_input_div>
-         <x-form_label for="client_code">
+        <x-form_label for="client_code">
             Client Code
         </x-form_label>
         <x-form_input id="client_code"
@@ -22,11 +22,18 @@
     </x-form_input_div>
 
     <x-form_input_div>
-      <x-form_label for="pronouns">
+        <x-form_label for="pronouns">
             Pronouns
         </x-form_label>
-        <select x-model="pronouns" id="pronouns" name="pronouns" class="w-full p-3 mt-2 border-b-2 border-blue-200 rounded-md peer ring-0" placeholder="Pronouns">
-            <option value="" disabled selected hidden>Pronouns:</option>
+        <select x-model="pronouns"
+            id="pronouns"
+            name="pronouns"
+            class="w-full p-3 mt-2 border-b-2 border-blue-200 rounded-md peer ring-0"
+            placeholder="Pronouns">
+            <option value=""
+                disabled
+                selected
+                hidden>Pronouns:</option>
             <option>He/Him</option>
             <option>She/Her</option>
             <option>They/Them</option>
@@ -35,9 +42,8 @@
         </select>
     </x-form_input_div>
 
-
     <x-form_input_div>
-         <x-form_label for="email">
+        <x-form_label for="email">
             Email
         </x-form_label>
         <x-form_input id="email"
@@ -46,8 +52,8 @@
             placeholder="email@example.com" />
     </x-form_input_div>
 
-     <x-form_input_div>
-         <x-form_label for="phone">
+    <x-form_input_div>
+        <x-form_label for="phone">
             Phone
         </x-form_label>
         <x-form_input id="phone"
@@ -57,26 +63,34 @@
     </x-form_input_div>
 
     <x-form_input_div>
-          <x-form_label for="contact_method">
+        <x-form_label for="contact_method">
             Contact Method
         </x-form_label>
         <select id="contact_method"
             type="text"
             name="contact_method"
             class="w-full p-3 mt-2 border-b-2 border-blue-200 rounded-md peer ring-0">
-              <option value="" disabled selected hidden>Preferred Contact Method</option>
+            <option value=""
+                disabled
+                selected
+                hidden>Preferred Contact Method</option>
             <option>Phone</option>
             <option>Text</option>
             <option>Email</option>
         </select>
     </x-form_input_div>
 
-        <x-form_input_div>
-      <x-form_label for="therapist">
+    <x-form_input_div>
+        <x-form_label for="therapist">
             Therapist
         </x-form_label>
-        <select id="therapist" name="therapist" class="w-full p-3 mt-2 border-b-2 border-blue-200 rounded-md peer ring-0">
-            <option value="" disabled selected hidden>Therapist</option>
+        <select id="therapist"
+            name="therapist"
+            class="w-full p-3 mt-2 border-b-2 border-blue-200 rounded-md peer ring-0">
+            <option value=""
+                disabled
+                selected
+                hidden>Therapist</option>
             @foreach ($therapists as $row)
                 <option value="{{ $row->id }}">
                     {{ $row->name }}
@@ -92,29 +106,3 @@
         </button>
     </div>
 </form>
-{{--
-<style>
-    select {
-        background: lightgray;
-        width: 100%;
-        border-bottom: solid;
-        focus
-
-        /* w-full h-10 text-gray-900 placeholder-transparent bg-transparent border-t-0 border-l-0 border-r-0 border-blue-300 border-b-1 peer placeholder:ml-2 focus:outline-none focus:ring-0 */
-    }
-
-    select:focus {
-    background-color: ivory;
-}
-
-
-select:focus-visible {
-    border: 2px dashed crimson;
-    border-radius: 3px;
-    outline: none;
-}
-
-    option {
-        color: white;
-    }
-</style> --}}
