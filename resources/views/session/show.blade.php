9b4271dbedc58fb1cc180501cdf73186f067369b
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-18 17:49:32 -0800

    add user to session.show, style, etc

--- resources/views/session/show.blade.php
+++ resources/views/session/show.blade.php
@@ -1,39 +1,48 @@
 <x-app-layout>
-    <x-slot name="header">
-        <p class="text-lg">Client: <span class="font-bold capitalize">{{ $therapySession->client->chosen_name }}
-        <p class="text-lg">Session Details</p>
-    </x-slot>
+    <x-main-container>
+        <x-container-header-secondary :user='$therapist'>
+            {{ $therapist->name }}
+        </x-container-header-secondary>
 
-    <section
-        class="w-5/6 h-full mx-auto text-gray-800 bg-gray-100 border border-black rounded-lg shadow-md shadow-blue-100 lg:w-1/2">
-        <div class="flex flex-row">
-            <div class="w-1/2 p-2 border-b border-r border-gray-700">Chosen Name:</div>
-            <div class="flex justify-between w-1/2 p-2 capitalize border-b border-gray-700">
-                <p>{{ $client->chosen_name }} </p>
-                <p class="mt-1 mr-2 text-xs">{{ $client->pronouns }}</p>
+
+        <section
+            class="w-5/6 h-full p-2 mx-auto mt-3 text-gray-800 bg-gray-100 border border-black rounded-lg shadow-md shadow-blue-100 lg:w-1/2">
+               <div>
+            <p class="text-lg">Client: <span class="font-bold capitalize">
+                {{ $therapySession->client->chosen_name }}
+            <p class="text-lg text-center">Session Details</p>
+        </div>
+            <div class="flex flex-row">
+                <div class="w-1/2 p-2 border-b border-r border-gray-400">Chosen Name:</div>
+                <div class="flex justify-between w-1/2 p-2 capitalize border-b border-gray-400">
+                    <p>{{ $client->chosen_name }} </p>
+                    <p class="mt-1 mr-2 text-xs">{{ $client->pronouns }}</p>
+                </div>
             </div>
-        </div>
-        <div class="flex flex-row">
-            <div class="w-1/2 p-2 border-b border-r border-gray-700">Session ID:</div>
-            <div class="w-1/2 p-2 border-b border-gray-700">{{ $therapySession->id }}</div>
-        </div>
-        <div class="flex flex-row">
-            <div class="w-1/2 p-2 border-b border-r border-gray-700">Session Date:</div>
-            <div class="w-1/2 p-2 border-b border-gray-700">{{ date('M d, Y, h:m', strtotime($therapySession->created_at)) }}
+            <div class="flex flex-row">
+                <div class="w-1/2 p-2 border-b border-r border-gray-400">Session ID:</div>
+                <div class="w-1/2 p-2 border-b border-gray-400">{{ $therapySession->id }}</div>
             </div>
+            <div class="flex flex-row">
+                <div class="w-1/2 p-2 border-b border-r border-gray-400">Session Date:</div>
+                <div class="w-1/2 p-2 border-b border-gray-400">
+                    {{ date('M d, Y, h:m', strtotime($therapySession->created_at)) }}
+                </div>
+            </div>
+            <div class="flex flex-row">
+                <div class="w-1/2 p-2 border-b border-r border-gray-400">Session Billed:</div>
+                <div class="w-1/2 p-2 border-b border-y-gray-400">{{ $therapySession->total_bill }}</div>
+            </div>
+            <div class="flex flex-row">
+                <div class="w-1/2 p-2 border-r border-gray-400">Insurance Coverage:</div>
+                <div class="w-1/2 p-2">{{ $therapySession->covered_cost }}</div>
+            </div>
+        </section>
+        <div class="flex mt-2">
+            <a href="{{ route('clients.show', $therapySession->client_id) }}"
+                class="mx-auto hover:text-blue-500">
+                Back to client Details
+            </a>
         </div>
-        <div class="flex flex-row">
-            <div class="w-1/2 p-2 border-b border-r border-gray-700">Session Billed:</div>
-            <div class="w-1/2 p-2 border-b border-y-gray-700">{{ $therapySession->total_bill }}</div>
-        </div>
-        <div class="flex flex-row">
-            <div class="w-1/2 p-2 border-r border-gray-700">Insurance Coverage:</div>
-            <div class="w-1/2 p-2">{{ $therapySession->covered_cost }}</div>
-        </div>
-    </section>
-    <div class="flex mt-2">
-        <a href="{{ route('clients.show', $therapySession->client_id) }}" class="mx-auto hover:text-blue-500">
-            Back to client Details
-        </a>
-    </div>
+    </x-main-container>
 </x-app-layout>
