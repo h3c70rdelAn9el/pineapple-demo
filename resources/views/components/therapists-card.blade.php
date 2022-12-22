869fc26c4e2540bcdfd384dec5f852506a77c333
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-22 09:39:05 -0800

    quick styling fix  to admin dashboard

--- resources/views/components/therapists-card.blade.php
+++ resources/views/components/therapists-card.blade.php
@@ -1,8 +1,8 @@
 <a href="{{ route('therapist.show', $therapist->id) }}"
-    class="flex flex-row">
+    class="h-16 m-2 text-center lg:h-12">
     <div
-        class="flex flex-row w-40 p-2 m-2 text-center transition-all duration-200 ease-in bg-blue-200 rounded-md shadow-md shadow-blue-100 hover:scale-105 hover:shadow-lg">
-        <p class="text-center capitalize">
+        class="flex flex-row w-full h-full p-2 text-center transition-all duration-200 ease-in bg-blue-200 rounded-md shadow-md lg:w-40 shadow-blue-100 hover:shadow-lg" >
+        <p class="m-auto text-center capitalize">
             {{ $therapist->name }}
         </p>
     </div>
