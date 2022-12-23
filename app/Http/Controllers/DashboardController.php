fe6cc7624cd9596a2882a2f95b6934d0a71398f2
h3c70rdelAn9el <hectordelangel@gmail.com>
2022-12-23 10:25:25 -0800

    display therapist on client card & add to dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,23 +15,18 @@ class DashboardController extends Controller
     public function index(User $user, TherapySession $therapySession)
     {
         $user = auth()->user();
-
         $user_id = $user->id;
 
         $clients = User::find($user_id)->clients;
+        $client = Client::find($user_id);
+        $allClients = Client::all();
 
         $therapySessions = TherapySession::where('user_id', $user->id)->get();
         $therapists = User::where('admin', 0)->get();
-        $therapist = TherapySession::where("user_id", "=", $user->id)->get();
-
-
-
-        $allClients = Client::all();
-
+        $therapist = User::find($user_id);
 
         if ($user->admin)
-
-            return view('dashboard_admin', ['user' => $user, 'therapists' => $therapists, 'allClients' => $allClients]);
+            return view('dashboard_admin', ['user' => $user, 'therapists' => $therapists, 'allClients' => $allClients, 'therapist' => $therapist]);
         else
             return view('dashboard', ['user' => $user, 'clients' => $clients, 'therapySessions' => $therapySessions, 'therapist' => $therapist]);
     }
