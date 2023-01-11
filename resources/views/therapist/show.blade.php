55fdc9947ada17d1345459317fc31b41ae797823
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-01-10 17:40:16 -0800

    add content component & refactor therapist page

--- resources/views/therapist/show.blade.php
+++ resources/views/therapist/show.blade.php
@@ -1,38 +1,37 @@
 <x-app-layout>
     <x-main-container>
         <x-container-header :user="$user">
-
         </x-container-header>
         <div class="w-2/3 p-2 m-2 mx-auto text-center bg-blue-100 rounded-lg lg:w-1/3">
             <h1 class="capitalize">Therapist: <span class="text-lg lg:font-bold">{{ $therapist->name }}</span></h1>
             {{-- TODO:  add credentials --}}
         </div>
-
-        <div class="flex flex-col-reverse w-full h-full max-w-6xl p-4 mx-auto mt-3 rounded-md md:flex-row">
-            <div class="p-2 m-2 bg-blue-100 rounded-md shadow-sm md:w-1/2">
-                <div class="flex flex-row flex-wrap justify-between mx-12 mb-2 text-lg border-b border-gray-100">
-                    <p class="font-bold">Clients:</p>
-                    <p class="ml-2">Total: <span class="font-bold">{{ $clients->count() }}</span></p>
-                </div>
-                <div class="flex flex-row flex-wrap p-2 m-2 mx-auto overflow-y-scroll h-96">
-
+        <div class="flex flex-col w-full h-full max-w-6xl p-4 mx-auto mt-3 rounded-md md:flex-row">
+            {{-- left/top --}}
+            <x-container-content :user="$user">
+                <x-slot name="title">
+                    Clients
+                </x-slot>
+                <x-slot name="count">
+                    {{ $clients->count() }}
+                </x-slot>
+                <x-slot name="content">
                     @foreach ($clients as $client)
                         <x-client-card :client="$client"
                             :therapist="$therapist"
                             :user="$user"></x-client-card>
                     @endforeach
-                </div>
-            </div>
-
-            {{-- right side --}}
-
-            <div class="p-2 m-2 bg-blue-100 rounded-md shadow-sm md:w-1/2">
-                <div
-                    class="flex flex-row flex-wrap justify-between mx-12 mb-2 overflow-y-scroll text-lg border-b border-gray-100">
-                    <p class="font-bold">Sessions:</p>
-                    <p class="ml-2">Total: <span class="font-bold">{{ $therapySessions->count() }}</span></p>
-                </div>
-                <div class="flex flex-row flex-wrap justify-center mx-auto overflow-y-scroll h-96">
+                </x-slot>
+            </x-container-content>
+            {{-- right/bottom --}}
+            <x-container-content>
+                <x-slot name="title">
+                    Sessions
+                </x-slot>
+                <x-slot name="count">
+                    {{ $therapySessions->count() }}
+                </x-slot>
+                <x-slot name="content">
                     @forelse ($therapySessions as $therapySession)
                         <x-session-card :therapySession='$therapySession'
                             :therapist='$therapist'
@@ -40,10 +39,8 @@
                     @empty
                         <p>There are no sessions to display</p>
                     @endforelse
-                </div>
-            </div>
-
+                </x-slot>
+            </x-container-content>
         </div>
-
     </x-main-container>
 </x-app-layout>
