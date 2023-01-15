5742b5e28093d66671f612ece1af54130f0e4a69
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-01-15 12:26:12 -0800

    add x-mask for form inputs & apply to session form

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -17,6 +17,10 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script defer src="https://unpkg.com/@alpinejs/mask@3.x.x/dist/cdn.min.js"></script>
+
+
     <!-- Styles -->
     @livewireStyles
 </head>
