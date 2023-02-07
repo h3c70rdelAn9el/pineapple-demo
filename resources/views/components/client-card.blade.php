bc710e1b2bf8ab533e2d688659032562810b46c9
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-02-06 16:41:38 -0800

    add padding to to client card

--- resources/views/components/client-card.blade.php
+++ resources/views/components/client-card.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ route('clients.show', $client->id) }}"
     {{-- class="flex flex-col justify-between w-full h-20 p-2 m-2 text-center transition-all duration-200 ease-in bg-blue-200 border border-transparent rounded-md shadow-md group shadow-blue-100 hover:border hover:border-blue-500 hover:shadow-lg"> --}}
-    class="relative flex flex-col justify-between h-20 p-2 m-2 transition-all duration-200 ease-in bg-blue-200 border border-blue-200 rounded-lg shadow-md shadow-blue-100 hover:border hover:border-blue-500 hover:shadow-lg">
+    class="relative flex flex-col justify-between h-full p-2 m-2 transition-all duration-200 ease-in bg-blue-200 border border-blue-200 rounded-lg shadow-md shadow-blue-100 hover:border hover:border-blue-500 hover:shadow-lg">
 
     <div class="-mt-1.5 flex w-full flex-row justify-between p-1">
         <p class="capitalize">{{ $client->preferred_name }}</p>
@@ -11,7 +11,7 @@
             <p class="text-sm text-left">Therapist: {{ $client->user->name }}</p>
         </div>
     @endif
-    <div class="p-1 pb-2">
+    <div class="p-1 -mb-1">
         <p class="text-xs text-left">
             Sessions: {{ $client->therapySessions->count() }}
             <span class="text-xs text-gray-600">/</span>
