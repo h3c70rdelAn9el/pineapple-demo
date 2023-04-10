c0a0b2ad64b74f1db3165e135f4c5c610267752a
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-04-10 12:41:00 -0700

    add search bar on main dashboard

--- resources/views/dashboard_admin.blade.php
+++ resources/views/dashboard_admin.blade.php
@@ -4,6 +4,30 @@
             {{ $user->name }}
         </x-container-header>
 
+        {{-- todo: ask if admin needs to notify of vacation , then uncomment or delete--}}
+        {{-- <div class="flex flex-row pt-2 ml-7">
+            <p class="ml-2">
+                {{ $user->on_vacation ? 'On Vacation' : 'Available' }}
+            </p>
+        </div> --}}
+
+        <div class="w-1/2 mx-auto mt-2">
+            <form action="/search"
+                method="get">
+                @csrf
+                <div class="flex flex-row">
+                    <input type="text"
+                        placeholder="Search for..."
+                        id="query"
+                        name="query"
+                        class="block w-full rounded-md"
+                        {{-- value={{ request()->get('query') }} --}}>
+                    <button type="submit"
+                        class="px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-700">Search</button>
+                </div>
+            </form>
+        </div>
+
         <div class="relative flex flex-col w-full h-full max-w-6xl p-4 mx-auto mt-3 rounded-md md:flex-row">
             <x-container-content>
                 <x-slot name="title">
@@ -35,12 +59,15 @@
                 </x-slot>
             </x-container-content>
         </div>
-        <div x-data="{ open:  false }">
+        <div x-data="{ open: false }">
             <button @click="open = !open"
                 class="text-lg text-center text-blue-400 hover:text-blue-600">Add Client</button>
-            <div x-show="open" x-cloak @click.away="open = false">
-                <div class="absolute inset-0 w-2/3 mx-auto top-6 ">
-                    <x-client-form :therapists="$therapists" :states="$states"></x-client-form>
+            <div x-show="open"
+                x-cloak
+                @click.away="open = false">
+                <div class="absolute inset-0 w-2/3 mx-auto top-6">
+                    <x-client-form :therapists="$therapists"
+                        :states="$states"></x-client-form>
                 </div>
             </div>
         </div>
