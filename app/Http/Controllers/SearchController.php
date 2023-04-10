1703e9eda49f387a6a87f1f174faf2e0b3297ed5
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-04-10 12:13:30 -0700

    add search for clients & users in search page

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class SearchController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        return view('search');
+        $results = null;
+
+
+        // if ($query = $request->get('query')) {
+        //     $results = Client::search($query)->get();
+        // }
+
+        if ($query = $request->get('query')) {
+            $clientResults = Client::search($query)->get();
+            $userResults = User::search($query)->get();
+            $results = $clientResults->merge($userResults);
+        }
+
+
+
+        // return view('search', [
+        //     'results' => $results,
+        // ]);
+
+        return view('search', [
+            'results' => $results,
+            'query' => $query,
+        ]);
     }
 }
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,12 +1,49 @@
     search
-<x-app-layout>
-    <div class="w-1/2">
-        <form action="#" method="get">
-            @csrf
-            <div class="flex flex-row">
-                <input type="text" placeholder="Search for..." id="query" name="query" class="block w-full rounded-md">
-                <button type="submit" class="px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-700">Search</button>
-            </div>
-        </form>
-    </div>
-</x-app-layout>
+    <x-app-layout>
+        <div class="w-1/2">
+            <form action="/search"
+                method="get">
+                @csrf
+                <div class="flex flex-row">
+                    <input type="text"
+                        placeholder="Search for..."
+                        id="query"
+                        name="query"
+                        class="block w-full rounded-md"
+                        value={{ request()->get('query') }}>
+                    <button type="submit"
+                        class="px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-700">Search</button>
+                </div>
+            </form>
+            @if ($results)
+                <div>
+                    @if (count($results) > 0)
+                        {{-- <h2>Client Results</h2> --}}
+                        @foreach ($results as $result)
+                            @if ($result instanceof \App\Models\Client)
+                                <div class="flex flex-row">
+                                    <p class="mr-2 font-bold">Client:</p>
+                                    <p>{{ $result->preferred_name }}</p>
+                                </div>
+                            @endif
+                        @endforeach
+
+                        {{-- <h2>User Results</h2> --}}
+                        @foreach ($results as $result)
+                            @if ($result instanceof \App\Models\User)
+                                <div class="flex flex-row">
+                                    <p class="mr-2 font-bold">Therapist:</p>
+                                    <p>{{ $result->name }}</p>
+                                </div>
+                            @endif
+                        @endforeach
+                    @else
+                        <p>No results found for "{{ $query }}".</p>
+                    @endif
+
+                </div>
+
+            @endif
+
+        </div>
+    </x-app-layout>
