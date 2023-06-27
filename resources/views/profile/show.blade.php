80a39e82e76bb37cfe9ed7c995c21eee744ca71e
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-06-27 12:29:43 -0700

    add upload form component to profile show page

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -6,53 +6,44 @@
     </x-slot>
 
     @if (session('alert'))
-        <div class="alert alert-{{ session('alert') }} alert-dismissible fade show"
-            role="alert"
-            x-data="{ show: true }"
-            x-show="show"
-            >
-            <div class="flex justify-between w-1/2 p-2 mx-auto rounded-md shadow-md">
-                <p>{{ session('message') }}</p>
-                <button>
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                        class="w-6 h-6 text-red-600 hover:text-red-800"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        @click="show = false"
-                        >
-                        <path stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
+    <div class="alert alert-{{ session('alert') }} alert-dismissible fade show" role="alert" x-data="{ show: true }" x-show="show">
+        <div class="flex justify-between w-1/2 p-2 mx-auto rounded-md shadow-md">
+            <p>{{ session('message') }}</p>
+            <button>
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-600 hover:text-red-800" fill="none" viewBox="0 0 24 24" stroke="currentColor" @click="show = false">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
         </div>
+    </div>
     @endif
 
     <div>
         <div class="py-10 mx-auto max-w-7xl sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
-                @livewire('profile.update-profile-information-form')
-
-                <x-jet-section-border />
+            @livewire('profile.update-profile-information-form')
+            <x-jet-section-border />
             @endif
 
-            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.update-password-form')
-                </div>
+            <x-file-upload />
 
-                <x-jet-section-border />
+            <x-jet-section-border />
+
+
+            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+            <div class="mt-10 sm:mt-0">
+                @livewire('profile.update-password-form')
+            </div>
+
+            <x-jet-section-border />
             @endif
 
             @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.two-factor-authentication-form')
-                </div>
+            <div class="mt-10 sm:mt-0">
+                @livewire('profile.two-factor-authentication-form')
+            </div>
 
-                <x-jet-section-border />
+            <x-jet-section-border />
             @endif
 
             <div class="mt-10 sm:mt-0">
@@ -60,12 +51,15 @@
             </div>
 
             @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
-                <x-jet-section-border />
+            <x-jet-section-border />
 
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.delete-user-form')
-                </div>
+            <div class="mt-10 sm:mt-0">
+                @livewire('profile.delete-user-form')
+            </div>
             @endif
         </div>
+
+
+
     </div>
 </x-app-layout>
--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -10,14 +10,9 @@
     <x-slot name="form">
         <!-- Profile Photo -->
         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-            <div x-data="{ photoName: null, photoPreview: null }"
-                class="col-span-6 sm:col-span-4">
-                <!-- Profile Photo File Input -->
-                <input type="file"
-                    class="hidden"
-                    wire:model="photo"
-                    x-ref="photo"
-                    x-on:change="
+        <div x-data="{ photoName: null, photoPreview: null }" class="col-span-6 sm:col-span-4">
+            <!-- Profile Photo File Input -->
+            <input type="file" class="hidden" wire:model="photo" x-ref="photo" x-on:change="
                                     photoName = $refs.photo.files[0].name;
                                     const reader = new FileReader();
                                     reader.onload = (e) => {
@@ -26,174 +21,114 @@
                                     reader.readAsDataURL($refs.photo.files[0]);
                             " />
 
-                <x-jet-label for="photo"
-                    value="{{ __('Photo') }}" />
+            <x-jet-label for="photo" value="{{ __('Photo') }}" />
 
-                <!-- Current Profile Photo -->
-                <div class="mt-2"
-                    x-show="! photoPreview">
-                    <img src="{{ $this->user->profile_photo_url }}"
-                        alt="{{ $this->user->name }}"
-                        class="object-cover w-20 h-20 rounded-full">
-                </div>
-
-                <!-- New Profile Photo Preview -->
-                <div class="mt-2"
-                    x-show="photoPreview"
-                    style="display: none;">
-                    <span class="block w-20 h-20 bg-center bg-no-repeat bg-cover rounded-full"
-                        x-bind:style="'background-image: url(\'' + photoPreview + '\');'">
-                    </span>
-                </div>
-
-                <x-jet-secondary-button class="mt-2 mr-2"
-                    type="button"
-                    x-on:click.prevent="$refs.photo.click()">
-                    {{ __('Select A New Photo') }}
-                </x-jet-secondary-button>
-
-                @if ($this->user->profile_photo_path)
-                    <x-jet-secondary-button type="button"
-                        class="mt-2"
-                        wire:click="deleteProfilePhoto">
-                        {{ __('Remove Photo') }}
-                    </x-jet-secondary-button>
-                @endif
-
-                <x-jet-input-error for="photo"
-                    class="mt-2" />
+            <!-- Current Profile Photo -->
+            <div class="mt-2" x-show="! photoPreview">
+                <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="object-cover w-20 h-20 rounded-full">
             </div>
+
+            <!-- New Profile Photo Preview -->
+            <div class="mt-2" x-show="photoPreview" style="display: none;">
+                <span class="block w-20 h-20 bg-center bg-no-repeat bg-cover rounded-full" x-bind:style="'background-image: url(\'' + photoPreview + '\');'">
+                </span>
+            </div>
+
+            <x-jet-secondary-button class="mt-2 mr-2" type="button" x-on:click.prevent="$refs.photo.click()">
+                {{ __('Select A New Photo') }}
+            </x-jet-secondary-button>
+
+            @if ($this->user->profile_photo_path)
+            <x-jet-secondary-button type="button" class="mt-2" wire:click="deleteProfilePhoto">
+                {{ __('Remove Photo') }}
+            </x-jet-secondary-button>
+            @endif
+
+            <x-jet-input-error for="photo" class="mt-2" />
+        </div>
         @endif
 
         <!-- Name -->
         <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="name"
-                value="{{ __('Name') }}" />
-            <x-jet-input id="name"
-                type="text"
-                class="block w-full mt-1"
-                wire:model.defer="state.name"
-                autocomplete="name" />
-            <x-jet-input-error for="name"
-                class="mt-2" />
+            <x-jet-label for="name" value="{{ __('Name') }}" />
+            <x-jet-input id="name" type="text" class="block w-full mt-1" wire:model.defer="state.name" autocomplete="name" />
+            <x-jet-input-error for="name" class="mt-2" />
         </div>
 
         <!-- Email -->
         <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="email"
-                value="{{ __('Email') }}" />
-            <x-jet-input id="email"
-                type="email"
-                class="block w-full mt-1"
-                wire:model.defer="state.email" />
-            <x-jet-input-error for="email"
-                class="mt-2" />
+            <x-jet-label for="email" value="{{ __('Email') }}" />
+            <x-jet-input id="email" type="email" class="block w-full mt-1" wire:model.defer="state.email" />
+            <x-jet-input-error for="email" class="mt-2" />
 
             @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::emailVerification()) &&
-                    !$this->user->hasVerifiedEmail())
-                <p class="mt-2 text-sm">
-                    {{ __('Your email address is unverified.') }}
+            !$this->user->hasVerifiedEmail())
+            <p class="mt-2 text-sm">
+                {{ __('Your email address is unverified.') }}
 
-                    <button type="button"
-                        class="text-sm text-gray-600 underline hover:text-gray-900"
-                        wire:click.prevent="sendEmailVerification">
-                        {{ __('Click here to re-send the verification email.') }}
-                    </button>
-                </p>
+                <button type="button" class="text-sm text-gray-600 underline hover:text-gray-900" wire:click.prevent="sendEmailVerification">
+                    {{ __('Click here to re-send the verification email.') }}
+                </button>
+            </p>
 
-                @if ($this->verificationLinkSent)
-                    <p v-show="verificationLinkSent"
-                        class="mt-2 text-sm font-medium text-green-600">
-                        {{ __('A new verification link has been sent to your email address.') }}
-                    </p>
-                @endif
+            @if ($this->verificationLinkSent)
+            <p v-show="verificationLinkSent" class="mt-2 text-sm font-medium text-green-600">
+                {{ __('A new verification link has been sent to your email address.') }}
+            </p>
+            @endif
             @endif
 
             {{-- License --}}
             <div class="col-span-6 mt-4 sm:col-span-4">
-                <x-jet-label for="license"
-                    value="{{ __('License') }}" />
-                <x-jet-input id="license"
-                    type="text"
-                    class="block w-full mt-1"
-                    wire:model.defer="state.license"
-                    autocomplete="license" />
-                <x-jet-input-error for="license"
-                    class="mt-2" />
+                <x-jet-label for="license" value="{{ __('License') }}" />
+                <x-jet-input id="license" type="text" class="block w-full mt-1" wire:model.defer="state.license" autocomplete="license" />
+                <x-jet-input-error for="license" class="mt-2" />
             </div>
 
             {{-- Expires at --}}
             <div class="col-span-6 mt-4 sm:col-span-4">
-                <x-jet-label for="expires_at"
-                    value="{{ __('Expires at') }}" />
-                <x-jet-input id="expires_at"
-                    type="date"
-                    class="block w-full mt-1"
-                    wire:model.defer="state.expires_at"
-                    autocomplete="expires_at" />
-                <x-jet-input-error for="expires_at"
-                    class="mt-2" />
+                <x-jet-label for="expires_at" value="{{ __('Expires at') }}" />
+                <x-jet-input id="expires_at" type="date" class="block w-full mt-1" wire:model.defer="state.expires_at" autocomplete="expires_at" />
+                <x-jet-input-error for="expires_at" class="mt-2" />
             </div>
 
             {{-- Bank Information --}}
             {{-- Bank Name --}}
             <div class="col-span-6 mt-4 sm:col-span-4">
-                <x-jet-label for="bank_name"
-                    value="{{ __('Bank Name') }}" />
-                <x-jet-input id="bank_name"
-                    type="text"
-                    class="block w-full mt-1"
-                    wire:model.defer="state.bank_name"
-                    autocomplete="bank_name" />
-                <x-jet-input-error for="bank_name"
-                    class="mt-2" />
+                <x-jet-label for="bank_name" value="{{ __('Bank Name') }}" />
+                <x-jet-input id="bank_name" type="text" class="block w-full mt-1" wire:model.defer="state.bank_name" autocomplete="bank_name" />
+                <x-jet-input-error for="bank_name" class="mt-2" />
             </div>
 
             {{-- Account Number --}}
             <div class="col-span-6 mt-4 sm:col-span-4">
-                <x-jet-label for="account_number"
-                    value="{{ __('Account Number') }}" />
-                <x-jet-input id="account_number"
-                    type="text"
-                    class="block w-full mt-1"
-                    wire:model.defer="state.account_number"
-                    autocomplete="account_number" />
-                <x-jet-input-error for="account_number"
-                    class="mt-2" />
+                <x-jet-label for="account_number" value="{{ __('Account Number') }}" />
+                <x-jet-input id="account_number" type="text" class="block w-full mt-1" wire:model.defer="state.account_number" autocomplete="account_number" />
+                <x-jet-input-error for="account_number" class="mt-2" />
             </div>
 
             {{-- Certificate File Upload --}}
             <div class="flex flex-col w-2/3 mt-3">
                 <p>Upload your documents:</p>
-                <div class="mt-3 ml-1">
-                    <a href="{{ route('fileUpload') }}">
-                        <button type="button"
-                            class="px-2 py-1 text-sm transition-all duration-200 bg-blue-800 rounded-md text-gray-50 hover:bg-blue-500">
-                            {{ __('Certification') }}
-                        </button>
-                    </a>
+
+                {{-- Certificate File Upload --}}
+                <div class="col-span-6 mt-4 sm:col-span-4">
+                    <x-jet-label for="certificate" value="{{ __('Certification') }}" />
+                    <x-jet-input id="certificate" type="file" class="block w-full mt-1" wire:model="state.certificate" accept=".pdf,.jpg,.jpeg,.png" />
+                    <x-jet-input-error for="certificate" class="mt-2" />
                 </div>
+
                 {{-- W9 - File Upload --}}
-                <div class="mt-3 ml-1">
-                    <a href="{{ route('fileUpload') }}">
-                        <button type="button"
-                            class="px-2 py-1 mx-auto text-sm transition-all duration-200 bg-blue-800 rounded-md text-gray-50 hover:bg-blue-500">
-                            {{ __('W9') }}
-                        </button>
-                    </a>
+                <div class="col-span-6 mt-4 sm:col-span-4">
+                    <x-jet-label for="w9" value="{{ __('W9') }}" />
+                    <x-jet-input id="w9" type="file" class="block w-full mt-1" wire:model.defer="state.w9" accept=".pdf,.jpg,.jpeg,.png" />
+                    <x-jet-input-error for="w9" class="mt-2" />
                 </div>
 
                 {{-- on_vacation --}}
                 <div class="pt-1">
                     <label for="on_vacation" value="{{ __('') }}">On Vacation:</label>
-                    <input type="checkbox"
-                        class="rounded-md"
-                        name="on_vacation"
-                        id="on_vacation"
-                    wire:model.defer="state.on_vacation"
-                        value="1"
-                        {{ old('on_vacation', $this->user->on_vacation) ? 'checked' : '' }}
-                        >
+                    <input type="checkbox" class="rounded-md" name="on_vacation" id="on_vacation" wire:model.defer="state.on_vacation" value="1" {{ old('on_vacation', $this->user->on_vacation) ? 'checked' : '' }}>
 
                 </div>
             </div>
@@ -201,13 +136,11 @@
     </x-slot>
 
     <x-slot name="actions">
-        <x-jet-action-message class="mr-3"
-            on="saved">
+        <x-jet-action-message class="mr-3" on="saved">
             {{ __('Saved.') }}
         </x-jet-action-message>
 
-        <x-jet-button wire:loading.attr="disabled"
-            wire:target="photo">
+        <x-jet-button wire:loading.attr="disabled" wire:target="photo">
             {{ __('Save') }}
         </x-jet-button>
     </x-slot>
