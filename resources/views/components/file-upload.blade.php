83c11a5edbcbd962e1d60e212e115ba21c598e84
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-06-27 14:26:10 -0700

    add document_type to file uploads.

--- resources/views/components/file-upload.blade.php
+++ resources/views/components/file-upload.blade.php
@@ -26,6 +26,18 @@
                     <span class="text-red-900">{{ $message }}</span>
                     @enderror
                 </div>
+                <div class="flex flex-wrap mb-3">
+                    <div class="relative">
+                        <select name="document_type" id="document_type" class="block w-full px-3 py-2 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500">
+                            <option value="">Select Document Type</option>
+                            <option value="W9">W9</option>
+                            <option value="Certificate">Certificate</option>
+                        </select>
+                        {{-- <label for="document_type"></label>
+                        <input type="text" id="document_type"> --}}
+
+                    </div>
+                </div>
 
                 <div class="mb-3">
                     <button type="submit" class="w-40 p-2 m-2 text-center transition-all duration-200 ease-in bg-blue-200 rounded-md shadow-md shadow-blue-100 hover:bg-blue-400">
@@ -34,9 +46,10 @@
                 </div>
             </form>
         </div>
-
     </div>
 </div>
+
+
 
 <script>
     function imageViewer(src = "") {
