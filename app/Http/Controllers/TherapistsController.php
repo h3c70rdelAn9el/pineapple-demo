4063c5e6f7615954bca03f67e09788d7301e0aa6
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-07-06 14:44:31 -0700

    delete commented out code

--- app/Http/Controllers/TherapistsController.php
+++ app/Http/Controllers/TherapistsController.php
@@ -18,21 +18,6 @@ class TherapistsController extends Controller
         return view('therapist.index')->with(['therapists' => $therapists]);
     }
 
-    // public function show($id)
-    // {
-    //     $user = auth()->user();
-    //     $therapist = User::find($id);
-    //     $clients = $therapist->clients()->get();
-    //     $therapySessions = TherapySession::where("client_id", "=", $therapist->id)->get();
-
-    //     // show the uploaded forms from fileuploadcontroller
-    //     $file_name = FileUpload::find($id);
-
-
-    //     return view('therapist.show', ['therapist' => $therapist, 'therapySessions' => $therapySessions, 'clients' => $clients, 'user' => $user, 'file_name' => $file_name]);
-    // }
-
-
     public function show($id)
     {
         $user = auth()->user();
