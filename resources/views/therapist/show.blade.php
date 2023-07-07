2c1e5fe54debdfb8ac30264233ae3ec790218149
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-07-07 12:07:06 -0700

    make new therapist page for admin, displaying their info

--- resources/views/therapist/show.blade.php
+++ resources/views/therapist/show.blade.php
@@ -2,25 +2,197 @@
     <x-main-container>
         <x-container-header :user="$user">
         </x-container-header>
-        <div class="w-2/3 p-2 m-2 mx-auto text-center bg-blue-100 rounded-lg lg:w-1/3">
-            <h1 class="capitalize">Therapist: <span class="text-lg lg:font-bold">{{ $therapist->name }}</span></h1>
-            {{-- TODO:  add credentials --}}
-            <p class="ml-2 text-sm text-gray-700">
-                {{ $user->on_vacation ? 'On Vacation' : 'Available' }}
-            </p>
-            {{-- show the route for the therapist docs --}}
-            <a href="{{ route('therapist.forms', $therapist, $file_name) }}" class="text-blue-500 underline">Therapist Forms</a>
 
-            <div class="font-[400]">
-                <p><span>{{ $space_for_new_clients}} </span> of {{ $therapist->number_of_potential_clients }} openings remaining</p>
-            </div>
-
-
-
-        </div>
         <div class="flex flex-col w-full h-full max-w-6xl p-4 mx-auto mt-3 rounded-md md:flex-row">
             {{-- left/top --}}
             <x-container-content :user="$user">
+                <x-slot name="title">
+                    <div class="flex items-center justify-between w-full">
+                        <p>{{ $therapist->name }}</p><span class="text-sm">{{ $therapist->gender }}</span>
+                    </div>
+                </x-slot>
+                <x-slot name="count">
+                </x-slot>
+                <x-slot name="content">
+                    <div class="p-2 text-sm font-normal">
+                        <p>
+                            Therapist is <span>{{ $therapist->on_vacation ? 'On Vacation' : 'Available'  }}</span>
+                        </p>
+                        <p>
+                            {{ $therapist->clients->count() }} clients
+                        </p>
+                        <p><span>{{ $space_for_new_clients}} </span> of {{ $therapist->number_of_potential_clients }} openings remaining</p>
+                        <div class="flex flex-row">
+                            @if ($therapist->home_address_state)
+                            <p>{{ $therapist->home_address_state }},</p>
+                            @else
+                            <p class="mr-2">California,</p>
+                            @endif
+
+                            @if ($therapist->home_address_country)
+                            <p>{{ $therapist->home_address_country }}</p>
+                            @else
+                            <p class="mr-2">US</p>
+                            @endif
+
+                            @if ($therapist->timezone)
+                            <p>{{ $therapist->timezone }}</p>
+                            @else
+                            <p class="mt-[1px] text-sm">(PST)</p>
+                            @endif
+                        </div>
+                        <div>
+                            @if ($therapist->email)
+                            <p>{{ $therapist->email }}</p>
+                            @else
+                            <p>Email needed</p>
+                            @endif
+                        </div>
+                        <div>
+                            @if ($therapist->phone)
+                            <p>{{ $therapist->phone }}</p>
+                            @else
+                            <p>Phone needed</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->clinical_license)
+                            <p>Clinical License: {{ $therapist->clinical_license }}</p>
+                            @else
+                            <p>Clinical license needed</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->annual_contact_about_complaints_uk_date )
+                            <p>UK complaints date: {{ $therapist->annual_contact_about_complaints_uk_date }}</p>
+                            @else
+                            <p>UK Complaints Date not provided</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->state_license_board)
+                            @if (filter_var($therapist->state_license_board, FILTER_VALIDATE_URL))
+                            <p>State License Board: <a href="{{ $therapist->state_license_board }}">{{ $therapist->astate_license_board }}</a></p>
+                            @else
+                            <p>State License Board: {{ $therapist->state_license_board }}</p>
+                            @endif
+                            @else
+                            <p>State License Board not provided</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->insurance)
+                            <p>Insurance: {{ $therapist->insurance }}</p>
+                            @else
+                            <p>Insurance needed</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->signed_documents)
+                            <p>Signed Documents: {{ $therapist->signed_documents }}</p>
+                            @else
+                            <p>Signed Documents needed</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->leah_signed)
+                            <p>Leah Signature: {{ $therapist->leah_signed }}</p>
+                            @else
+                            <p>Leah Signature needed</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->out_of_state_coaching)
+                            <p>Out of State Coaching: {{ $therapist->out_of_state_coaching }}</p>
+                            @else
+                            <p>Out of State Coaching n/a</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->w9)
+                            <p>W9 recieved: {{ $therapist->w9 }}</p>
+                            @else
+                            <p>W9 needed</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->voided_cheque)
+                            <p>Voided Cheque recieved: {{ $therapist->voided_cheque }}</p>
+                            @else
+                            <p>Voided Cheque needed</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->bio)
+                            <p>Bio: {{ $therapist->bio }}</p>
+                            @else
+                            <p>Bio n/a</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->website)
+                            <p>Website: {{ $therapist->website }}</p>
+                            @else
+                            <p>Website n/a</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->quickbooks)
+                            <p>Quickbooks: {{ $therapist->quickbooks }}</p>
+                            @else
+                            <p>Quickbooks n/a</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->dropbox)
+                            <p>Dropbox: {{ $therapist->dropbox }}</p>
+                            @else
+                            <p>Dropbox n/a</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->client_extensions)
+                            <p>Client Extensions: {{ $therapist->client_extensions }}</p>
+                            @else
+                            <p>Client Extensions n/a</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->notes)
+                            <p>Notes: {{ $therapist->notes }}</p>
+                            @else
+                            <p>Notes not provided</p>
+                            @endif
+                        </div>
+
+                        <div>
+                            @if ($therapist->covid_fundraise)
+                            <p>Covid Fundraise: {{ $therapist->covid_fundraise }}</p>
+                            @else
+                            <p>Covid Fundraise n/a</p>
+                            @endif
+                        </div>
+                    </div>
+                </x-slot>
+            </x-container-content>
+
+            {{-- right/bottom --}}
+            <x-container-content>
                 <x-slot name="title">
                     Clients
                 </x-slot>
@@ -31,26 +203,6 @@
                     @foreach ($clients as $client)
                     <x-client-card :client="$client" :therapist="$therapist" :user="$user"></x-client-card>
                     @endforeach
-                </x-slot>
-            </x-container-content>
-            {{-- right/bottom --}}
-            <x-container-content>
-                <x-slot name="title">
-                    Sessions
-                </x-slot>
-                <x-slot name="count">
-                    {{ $clients->sum(function ($client) {
-                        return $client->therapySessions->count();
-                    }) }}
-                </x-slot>
-                <x-slot name="content">
-                    @forelse ($clients as $client)
-                    @foreach ($client->therapySessions as $therapySession)
-                    <x-session-card :therapySession='$therapySession' :therapist='$therapist' :client='$client'></x-session-card>
-                    @endforeach
-                    @empty
-                    <p>There are no sessions to display</p>
-                    @endforelse
                 </x-slot>
             </x-container-content>
         </div>
