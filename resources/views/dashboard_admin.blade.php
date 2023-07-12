aa888658c038994cf0d5796a2a99c5e81f73ad42
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-07-12 09:42:40 -0700

    move the add clients button to top. remove some comments

--- resources/views/dashboard_admin.blade.php
+++ resources/views/dashboard_admin.blade.php
@@ -4,26 +4,18 @@
             {{ $user->name }}
         </x-container-header>
 
-        {{-- todo: ask if admin needs to notify of vacation , then uncomment or delete--}}
         {{-- <div class="flex flex-row pt-2 ml-7">
             <p class="ml-2">
                 {{ $user->on_vacation ? 'On Vacation' : 'Available' }}
-            </p>
+        </p>
         </div> --}}
 
         <div class="w-1/2 mx-auto mt-2">
-            <form action="/search"
-                method="get">
+            <form action="/search" method="get">
                 @csrf
                 <div class="flex flex-row">
-                    <input type="text"
-                        placeholder="Search for..."
-                        id="query"
-                        name="query"
-                        class="block w-full rounded-md"
-                        {{-- value={{ request()->get('query') }} --}}>
-                    <button type="submit"
-                        class="px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-700">Search</button>
+                    <input type="text" placeholder="Search for..." id="query" name="query" class="block w-full rounded-md" {{-- value={{ request()->get('query') }} --}}>
+                    <button type="submit" class="px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-700">Search</button>
                 </div>
             </form>
         </div>
@@ -38,55 +30,39 @@
                 </x-slot>
                 <x-slot name="content">
                     @foreach ($therapists as $therapist)
-                        <x-therapists-card :therapist="$therapist"></x-therapists-card>
+                    <x-therapists-card :therapist="$therapist"></x-therapists-card>
                     @endforeach
                 </x-slot>
             </x-container-content>
 
             <x-container-content>
                 <x-slot name="title">
-                    Clients:
+                    Clients: {{ $allClients->count() }}
                 </x-slot>
                 <x-slot name="count">
-                    {{ $allClients->count() }}
-                </x-slot>
-                <x-slot name="content">
-                <div x-data="{ open: false }">
-                    <button @click="open = !open" class="text-lg text-center text-blue-400 hover:text-blue-600">Add Client</button>
-                    <div x-show="open" x-cloak @click.away="open = false">
-                        <div class="absolute inset-0 w-2/3 mx-auto top-6">
-                            <x-client-form :therapists="$therapists" :states="$states" :categories="$categories"></x-client-form>
+                    <div x-data="{ open: false }">
+                        <button @click="open = !open" class="text-lg text-center text-blue-400 hover:text-blue-600">Add Client</button>
+                        <div x-show="open" x-cloak @click.away="open = false">
+                            <div class="absolute inset-0 w-2/3 mx-auto top-6">
+                                <x-client-form :therapists="$therapists" :states="$states" :categories="$categories"></x-client-form>
+                            </div>
                         </div>
                     </div>
-                </div>
-                {{-- add theroute for add client --}}
-                <a href="{{ route('clients.create') }}"
-                    class="h-6 ml-10 text-sm text-blue-600 hover:text-blue-800">
-                    Add Client
-                </a>
 
+                    {{-- TODO:  this link gives a 'user_id cannot be null' error --}}
+                       {{-- <a href="{{ route('clients.create') }}"
+                       class="h-6 ml-10 text-sm text-blue-600 hover:text-blue-800">
+                       Add Client
+                       </a> --}}
 
+                </x-slot>
+
+                <x-slot name="content">
                     @foreach ($allClients as $client)
-                        <x-client-card :client="$client"
-                            :therapist="$therapist"
-                            :user="$user"></x-client-card>
+                    <x-client-card :client="$client" :therapist="$therapist" :user="$user"></x-client-card>
                     @endforeach
                 </x-slot>
             </x-container-content>
         </div>
-
-        {{-- <div x-data="{ open: false }">
-            <button @click="open = !open"
-                class="text-lg text-center text-blue-400 hover:text-blue-600">Add Client</button>
-            <div x-show="open"
-                x-cloak
-                @click.away="open = false">
-                <div class="absolute inset-0 w-2/3 mx-auto top-6">
-                    <x-client-form :therapists="$therapists"
-                        :states="$states" :categories="$categories"></x-client-form>
-                </div>
-            </div>
-        </div> --}}
-
     </x-main-container>
 </x-app-layout>
