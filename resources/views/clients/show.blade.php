64f13b445ee9ca2066879d8d99d6be013eb7792b
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-07-25 17:20:27 -0700

    display sessions left on client page

--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -33,9 +33,15 @@
                 </div>
             </div>
             {{-- right side --}}
-            <div class="p-1 mx-auto mt-2 mb-2 rounded-md shadow-md bg-blue-50 shadow-blue-100 lg:w-1/2 lg:p-2">
-                <h2 class="text-lg font-bold text-center">Client Sessions:</h2>
-                <div class="flex flex-row flex-wrap p-2 mx-auto overflow-y-scroll h-96">
+            <div class="w-5/6 p-1 mx-auto mt-2 rounded-md shadow-md bg-blue-50 shadow-blue-100 lg:w-1/2 lg:p-2">
+                <h2 class="text-lg font-bold text-center">Client Sessions</h2>
+                <div class="flex flex-row mt-2">
+                    <p class="text-xs text-gray-500">Sessions Left:
+                        <span class="font-bold">{{ $client->max_sessions - $client->therapySessions->count() }}</span>
+                        out of <span class="font-bold">{{ $client->max_sessions  }}</span>
+                    </p>
+                </div>
+                <div class="flex flex-row flex-wrap w-full p-2 mx-auto overflow-y-scroll border border-indigo-500 rounded-md shadow-md h-96">
                     @forelse ($client->therapySessions as $therapySession)
                         <x-session-card :therapySession='$therapySession'
                             :therapist='$therapist' />
