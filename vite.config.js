d71752180d3dd49ff360943b7e036fb9bed6d958
Tanguy de Courson <tanguy@0x7a69.com>
2023-07-27 07:21:53 -0700

    hopefully get rid of this error

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: [
                 "resources/css/app.css",
                 "resources/js/app.js",
-                "resources/img/**",
+                /*"resources/img/**",*/
                 "resources/css/intlTelInput.css",
                 "resources/js/intlTelInput.js",
             ],
