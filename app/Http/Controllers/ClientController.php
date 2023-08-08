44cc25f47e06b020f370e6456048419201d49314
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-08-07 22:14:51 -0700

    add proper limit to therapySession count & display on show page

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -173,11 +173,14 @@ class ClientController extends Controller
     {
         $client = Client::find($id, ['*'], 'preferred_name', 'asc');
         $therapySessions = TherapySession::all();
+        $attendedSessions = $client->therapySessions()
+            ->whereIn('attendance', ['attended', 'no-show'])
+            ->get();
         $user_id = $client->user_id;
         // $therapist = User::find($user_id);
         $therapist = User::where('id', $user_id)->first();
 
-        return view('clients.show')->with(['client' => $client, 'therapySessions' => $therapySessions, 'therapist' => $therapist]);
+        return view('clients.show')->with(['client' => $client, 'therapySessions' => $therapySessions, 'therapist' => $therapist, 'attendedSessions' => $attendedSessions]);
     }
 
     /**
