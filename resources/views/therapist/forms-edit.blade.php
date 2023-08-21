631c00e697aa42caf9883277aaf09b6b0e0e7cab
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-08-21 15:32:57 -0700

    add download function to upladed forms

--- resources/views/therapist/forms-edit.blade.php
+++ resources/views/therapist/forms-edit.blade.php
@@ -1,15 +1,23 @@
 <x-app-layout>
     <h1>Edit Form</h1>
-
     <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
         <div class="items-center justify-center">
             @if (Str::contains($form->file_name, '.pdf'))
-            <embed src="{{ asset('uploads/forms/therapist/' . $form->file_name) }}" class="h-64 w-full rounded-md md:h-[300px] md:w-full" type="application/pdf" alt="{{ $form->file_name }}" />
-            @elseif (Str::contains($form->file_name, '.jpg') || Str::contains($form->file_name, '.png') || Str::contains($form->file_name, '.jpeg'))
-            <img src="{{ asset('uploads/forms/therapist/' . $form->file_name) }}" {{-- class="h-[300px] w-full rounded-md" --}} class="h-64 w-full rounded-md md:h-[300px] md:w-full" alt="{{ $form->file_name }}" />
+                <embed class="h-64 w-full rounded-md md:h-[300px] md:w-full"
+                    src="{{ asset('uploads/forms/therapist/' . $form->file_name) }}" type="application/pdf"
+                    alt="{{ $form->file_name }}" />
+            @elseif (Str::contains($form->file_name, '.jpg') ||
+                    Str::contains($form->file_name, '.png') ||
+                    Str::contains($form->file_name, '.jpeg') ||
+                    Str::contains($form->file_name, '.JPEG'))
+                <img class="h-64 w-full rounded-md md:h-[300px] md:w-full"
+                    src="{{ asset('uploads/forms/therapist/' . $form->file_name) }}" alt="{{ $form->file_name }}"
+                    {{-- class="h-[300px] w-full rounded-md" --}} />
             @endif
+            <a class="text-xs button" href="{{ asset('uploads/forms/therapist/' . $form->file_name) }}" download>
+                Download
+            </a>
         </div>
-
     </div>
 
     <div class="flex flex-row justify-between w-full">
@@ -19,9 +27,9 @@
                     <p class="text-xs font-light">Verified:</p>
                     <p class="pl-2 text-xs font-light">
                         @if ($form->verified == 1)
-                        Yes
+                            Yes
                         @else
-                        No
+                            No
                         @endif
                     </p>
                 </div>
@@ -42,16 +50,14 @@
         </div>
     </div>
     @if ($user->admin == 1)
-
-    <form action="{{ route('fileUpdate', $form->id) }}" method="POST">
-        @csrf
-        @method('PUT')
-        <input type="hidden" name="form_id" value="{{ $form->id }}">
-        <label for="verified">Verify</label>
-        <input type="checkbox" name="verified" id="verified">
-        <button type="submit">Submit</button>
-    </form>
+        <form action="{{ route('fileUpdate', $form->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <input name="form_id" type="hidden" value="{{ $form->id }}">
+            <label for="verified">Verify</label>
+            <input id="verified" name="verified" type="checkbox">
+            <button type="submit">Submit</button>
+        </form>
     @else
-
     @endif
 </x-app-layout>
