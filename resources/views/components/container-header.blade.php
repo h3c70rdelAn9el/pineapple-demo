a001f5776614b3ed66a3b9747ac7e060ba6f0b26
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-09-02 09:41:38 -0700

    fix therapist name on header section

--- resources/views/components/container-header.blade.php
+++ resources/views/components/container-header.blade.php
@@ -1,7 +1,6 @@
 <section
     class="flex flex-row items-center justify-around w-full mx-auto text-white bg-blue-500 rounded-t-md md:flex-row">
     <div class="flex flex-col p-4 text-center capitalize md:flex-row text-md md:text-xl">
-        <p class="">Welcome:</p>
         <p class="ml-1 font-bold md:ml-0">{{ $user->name }}</p>
     </div>
     <div class="bg-blue-500 md:w-44 min-w-min">
