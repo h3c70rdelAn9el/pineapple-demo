bfd59a1842b663dc276a76051ff8a051b38aa982
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-09-02 11:10:57 -0700

    fix a bug in client details, refactor session.show

--- resources/views/session/show.blade.php
+++ resources/views/session/show.blade.php
@@ -1,66 +1,44 @@
 <x-app-layout>
     <x-main-container>
-        <x-container-header-secondary :user='$user'>
+        <x-container-header-secondary :user="$user">
             {{ $user->name }}
         </x-container-header-secondary>
-        <section class="w-5/6 h-full p-2 mx-auto mt-3 text-gray-800 bg-gray-100 border border-black rounded-lg shadow-md shadow-blue-100 lg:w-1/2">
+        <section
+                 class="w-5/6 h-full p-2 mx-auto mt-3 text-gray-800 bg-gray-100 border border-black rounded-lg shadow-md shadow-blue-100 lg:w-1/2">
             <div>
-                <p class="text-lg">Client: <span class="font-bold capitalize">
-                        {{ $therapySession->client->preferred_name }}
-                        <p class="text-lg text-center">Session Details</p>
+                <p class="text-lg">Client: <span class="font-bold capitalize">{{ $therapySession->client->preferred_name
+                        }}</span></p>
+                <p class="text-lg text-center">Session Details</p>
             </div>
+            @foreach ([
+            'Preferred Name' => $client->preferred_name,
+            'Therapist' => $client->user->name,
+            'Session ID' => $therapySession->id,
+            'Session Date' => date('F d, Y', strtotime($therapySession->created_at)),
+            'Session Cost' => $therapySession->session_cost,
+            'Original Client Contribution' => $client->client_contribution,
+            'Remaining Client Contribution' => $therapySession->remaining_client_contribution,
+            'Session Attendance' => $therapySession->attendance,
+            ] as $label => $value)
             <div class="flex flex-row">
-                <div class="w-1/2 p-2 border-b border-r border-gray-400">Preferred Name:</div>
-                <div class="flex justify-between w-1/2 p-2 capitalize border-b border-gray-400">
-                    <p>{{ $client->preferred_name }} </p>
-                    <p class="mt-1 mr-2 text-xs">{{ $client->pronouns }}</p>
+                <div class="w-1/2 p-2 border-b border-r border-gray-400">{{ $label }}:</div>
+                <div
+                     class="@if ($label === 'Session Attendance') text-{{ $attendanceColor }} @endif w-1/2 border-b border-gray-400 p-2">
+                    @if ($label === 'Session Attendance')
+                    <span class="capitalize">{{ $value }}</span>
+                    @else
+                    {{ $value }}
+                    @endif
                 </div>
             </div>
+            @endforeach
             <div class="flex flex-row">
-                <div class="w-1/2 p-2 border-b border-r border-gray-400">Therapist:</div>
-                <div class="flex justify-between w-1/2 p-2 capitalize border-b border-gray-400">
-                    <p>{{ $client->user->name }} </p>
-                </div>
-            </div>
-            <div class="flex flex-row">
-                <div class="w-1/2 p-2 border-b border-r border-gray-400">Session ID:</div>
-                <div class="w-1/2 p-2 border-b border-gray-400">{{ $therapySession->id }}</div>
-            </div>
-            <div class="flex flex-row">
-                <div class="w-1/2 p-2 border-b border-r border-gray-400">Session Date:</div>
-                <div class="w-1/2 p-2 border-b border-gray-400">
-                    {{ date('F d, Y', strtotime($therapySession->created_at)) }}
-                </div>
-            </div>
-            <div class="flex flex-row">
-                <div class="w-1/2 p-2 border-b border-r border-gray-400">Session Cost:</div>
-                <div class="w-1/2 p-2 border-b border-gray-400">{{ $therapySession->session_cost }}</div>
-            </div>
-            <div class="flex flex-row">
-                <div class="w-1/2 p-2 border-b border-r border-gray-400">Original Client Contribution:</div>
-                <div class="w-1/2 p-2 border-b border-gray-400">{{ $client->client_contribution }}</div>
-            </div>
-            <div class="flex flex-row">
-                <div class="w-1/2 p-2 border-b border-r border-gray-400">Remaining Client Contribution:</div>
-                <div class="w-1/2 p-2 border-b border-gray-400">{{ $therapySession->remaining_client_contribution }}</div>
-            </div>
-            <div class="flex flex-row">
-                <div class="w-1/2 p-2 border-r border-gray-400">Session Attendance:</div>
-                @if ($therapySession->attendance === 'attended')
-                <div class="w-1/2 p-2 text-green-500 capitalize">{{ $therapySession->attendance }}</div>
-                @elseif ($therapySession->attendance === 'no-show')
-                <div class="w-1/2 p-2 text-red-500 capitalize">{{ $therapySession->attendance }}</div>
-                @elseif ($therapySession->attendance === 'canceled')
-                <div class="w-1/2 p-2 text-yellow-500 capitalize">{{ $therapySession->attendance }}</div>
-                @else
-                <div class="w-1/2 p-2 capitalize">{{ $therapySession->attendance }}</div>
-                @endif
+                <div class="w-1/2 p-2 border-r border-gray-400">Session Notes:</div>
+                <div class="w-1/2 p-2 border-gray-400">{{ $therapySession->notes }}</div>
             </div>
         </section>
         <div class="flex mt-2">
-            <a href="{{ route('clients.show', $therapySession->client_id) }}" class="mx-auto hover:text-blue-500">
-                Back to client Details
-            </a>
+            <a class="mx-auto hover:text-blue-500" href="{{ route('clients.show', $therapySession->client_id) }}">Back to client Details</a>
         </div>
     </x-main-container>
 </x-app-layout>
