e0034a0ab56264f73e6decd1899e369c223f982f
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-09-02 09:41:38 -0700

    fix therapist name on header section

--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -1,18 +1,7 @@
 <x-app-layout>
     <x-main-container>
-        <section
-            class="flex flex-row items-center justify-around w-full mx-auto text-white bg-blue-500 rounded-t-md md:flex-row">
-            <div class="p-4 text-center capitalize shadow-md sm:rounded-lg">
-                <img class="object-fill w-20 h-full mx-auto rounded-full shadow-md opacity-80"
-                    src="{{ asset('pineapple-logo-vertical-2.png') }}"
-                    alt="pineapple logo"
-                    {{-- src="{{ asset('images/pineapple.png') }}" --}}>
-
-            </div>
-            <div class="bg-blue-500 w-44">
-                <x-clock class="pr-2 font-bold text-right text-white bg-blue-500 lg:text-xl"></x-clock>
-            </div>
-        </section>
+            <x-container-header :user="$user">
+        </x-container-header>
         @if (Auth::user()->admin)
             <a class="h-6 ml-10 text-sm text-blue-600 hover:text-blue-800"
                 href="{{ route('clients.edit', $client->id) }}">
