5facd0696e8b80da4785f53d216844a101d58501
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-09-07 22:07:48 -0700

    add edit page/link for therapist..admin only

--- app/Http/Controllers/TherapistsController.php
+++ app/Http/Controllers/TherapistsController.php
@@ -49,12 +49,56 @@ class TherapistsController extends Controller
         $user = auth()->user();
         $therapist = User::find($id);
         $form = $therapist->therapist;
-
+        // dd($therapist);
         return view('therapist.edit', [
             'therapist' => $therapist,
             'user' => $user,
             'form' => $form,
         ]);
+    }
+
+    // give me the update function
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'title' => 'nullable|string|max:255',
+            'name' => 'nullable|string|max:255',
+            'preferred_name' => 'nullable|string|max:255',
+            'email' => 'nullable|string|email|max:255',
+            'gender' => 'nullable|string|max:255',
+            'intern' => 'nullable|string|max:255',
+            'supervisor_name' => 'nullable|string|max:255',
+            'street_address' => 'nullable|string|max:255',
+            'county/town' => 'nullable|string|max:255',
+            'state' => 'nullable|string|max:255',
+            'zip_code_postcode' => 'nullable|string|max:255',
+            'country' => 'nullable|string|max:255',
+            'time_zone' => 'nullable|string|max:255',
+            'account_name' => 'nullable|string|max:255',
+            'account_number' => 'nullable|string|max:255',
+            'routing_number' => 'nullable|string|max:255',
+            'iban_swift_code' => 'nullable|string|max:255',
+            'client_spaces' => 'nullable|string|max:255',
+            'full' => 'nullable|string|max:255',
+            'out_of_state_coaching' => 'nullable|string|max:255',
+            'contact_for_promotionals' => 'nullable|string|max:255',
+            'active_status' => 'nullable|string|max:255',
+            'contract_signed' => 'nullable|string|max:255',
+            'all_documents' => 'nullable|string|max:255',
+            'website' => 'nullable|string|max:255',
+            'quickbooks' => 'nullable|string|max:255',
+            'session_cost' => 'nullable|string|max:255',
+            'client_extensions' => 'nullable|string|max:255',
+            'notes' => 'nullable|string|max:255',
+        ]);
+
+        // $therapist = User::find($id);
+        // $form = $therapist->therapist;
+
+        User::find($id)->update($validatedData);
+
+        return redirect()->route('therapist.show', $id)->with('success', 'Therapist updated successfully');
     }
 
 }
--- resources/views/components/edit-form-input.blade.php
+++ resources/views/components/edit-form-input.blade.php
@@ -1,3 +1,4 @@
-@props(['disabled' => false])
+@props(['disabled' => false, 'value' => null])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => "w-full rounded-md border border-blue-200 bg-gray-50 py-1 px-3 text-base text-gray-600 outline-none focus:border-blue-500 focus:shadow-md font-normal"]) !!}>
+
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => "w-full rounded-md border border-blue-200 bg-gray-100 py-1 px-3 text-base text-gray-600 outline-none focus:border-blue-500 focus:shadow-md font-normal"]) !!} >
