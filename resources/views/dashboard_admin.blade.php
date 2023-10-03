a199e8cb067b256f6e39639a544c0a596a2f6d91
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-10-02 19:38:01 -0700

    add therapist count to admin dashboard

--- resources/views/dashboard_admin.blade.php
+++ resources/views/dashboard_admin.blade.php
@@ -10,61 +10,94 @@
         </p>
         </div> --}}
 
-        <div class="w-1/2 mx-auto mt-2">
-            <form action="/search" method="get">
+        <div class="mx-auto mt-2 w-1/2">
+            <form action="/search"
+                method="get">
                 @csrf
                 <div class="flex flex-row">
-                    <input type="text" placeholder="Search for..." id="query" name="query" class="block w-full rounded-md" {{-- value={{ request()->get('query') }} --}}>
-                    <button type="submit" class="px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-700">Search</button>
+                    <input class="block w-full rounded-md"
+                        id="query"
+                        name="query"
+                        type="text"
+                        placeholder="Search for..."
+                        {{-- value={{ request()->get('query') }} --}}>
+                    <button class="rounded-md bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                        type="submit">Search</button>
                 </div>
             </form>
         </div>
 
-        <div class="relative flex flex-col w-full h-full max-w-6xl p-4 mx-auto mt-3 rounded-md md:flex-row">
+        <div class="relative mx-auto mt-3 flex h-full w-full max-w-6xl flex-col rounded-md p-4 md:flex-row">
             <x-container-content>
                 <x-slot name="title">
-                    Therapists:
+                    {{-- Therapists: --}}
+
+                    <div class="flex w-1/4 flex-col text-base">
+                        <div class="flex justify-between font-bold">
+                            <p>Therapists:</p>
+                            <p>{{ $therapists->count() }}</p>
+                        </div>
+                        <div class="flex justify-between">
+                            <p>Active:</p>
+                            <p class='text-green-500'>{{ $activeTherapists->count() }}</p>
+                        </div>
+                        <div class="flex justify-between">
+                            <p>Inactive:</p>
+                            <p class="text-orange-500">{{ $inactiveTherapists->count() }}</p>
+                        </div>
+                    </div>
                 </x-slot>
                 <x-slot name="count">
-                    {{ $therapists->count() }}
+
+                    {{-- {{ $therapists->count() }}
+                    {{ $activeTherapists->count() }}
+                    {{ $inactiveTherapists->count() }} --}}
+                    {{-- <div class="flex flex-col">
+                        <p>Total: <span>{{ $therapists->count() }}</span></p>
+                        <p>Active: <span>{{ $activeTherapists->count() }}</span></p>
+                        <p>Inactive: <span>{{ $inactiveTherapists->count() }}</span> </p>
+                    </div> --}}
                 </x-slot>
                 <x-slot name="content">
                     @foreach ($therapists as $therapist)
-                    <x-therapists-card :therapist="$therapist"></x-therapists-card>
+                        <x-therapists-card :therapist="$therapist"></x-therapists-card>
                     @endforeach
                 </x-slot>
             </x-container-content>
 
             <x-container-content>
-                    <x-slot name="title" class="">
-                     <div class="flex flex-col w-1/4 text-base">
-                            <div class="flex justify-between">
-                                <p>Clients:</p>
-                                <p>{{ $allClients->count() }}</p>
-                            </div>
-                            <div class="flex justify-between">
-                                <p>Active:</p>
-                                <p class='text-green-500'>{{ $activeClients->count() }}</p>
-                            </div>
-                            <div class="flex justify-between">
-                                <p>Inactive:</p>
-                                <p class="text-orange-500">{{ $inactiveClients->count() }}</p>
-                            </div>
-                     </div>
+                <x-slot
+                    name="title">
+                    <div class="flex w-1/4 flex-col text-base">
+                        <div class="flex justify-between font-bold">
+                            <p>Clients:</p>
+                            <p>{{ $allClients->count() }}</p>
+                        </div>
+                        <div class="flex justify-between">
+                            <p>Active:</p>
+                            <p class='text-green-500'>{{ $activeClients->count() }}</p>
+                        </div>
+                        <div class="flex justify-between">
+                            <p>Inactive:</p>
+                            <p class="text-orange-500">{{ $inactiveClients->count() }}</p>
+                        </div>
+                    </div>
 
-                    </x-slot>
-                    <x-slot name="count">
-                        <button class="mt-4 button-secondary">
-                            <a href="{{ route('clients.create') }}" class="text-sm ">
-                                Add Client
-                            </a>
-                        </button>
-                    </x-slot>
+                </x-slot>
+                <x-slot name="count">
+                    <button class="button-secondary mt-4">
+                        <a class="text-sm"
+                            href="{{ route('clients.create') }}">
+                            Add Client
+                        </a>
+                    </button>
+                </x-slot>
 
                 <x-slot name="content">
                     @foreach ($allClients as $client)
-                    <x-client-card :client="$client" :therapist="$therapist" :user="$user"
-                    ></x-client-card>
+                        <x-client-card :client="$client"
+                            :therapist="$therapist"
+                            :user="$user"></x-client-card>
                     @endforeach
                 </x-slot>
             </x-container-content>
