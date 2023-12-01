25dadde8f29402232624c90040fce7e22e02af30
Hector del Angel <hectordelangel@gmail.com>
2023-12-01 10:06:54 -0800

    make clients display alphabetically on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,11 +15,11 @@ class DashboardController extends Controller
     {
         $user = auth()->user();
         $user_id = $user->id;
-        $clients = User::find($user_id)->clients;
+        $clients = User::find($user_id)->clients()->orderBy('preferred_name')->get();
         $client = Client::find($user_id);
         $allClients = Client::all()->sortBy('preferred_name');
-        $activeClients = Client::where('status', '0')->get();
-        $inactiveClients = Client::where('status', '1')->get();
+        $activeClients = Client::where('status', '0')->get()->sortBy('preferred_name');
+        $inactiveClients = Client::where('status', '1')->get()->sortBy('preferred_name');
         $therapySessions = TherapySession::where('user_id', $user->id)
             ->orderBy('id', 'DESC')
             ->get();
