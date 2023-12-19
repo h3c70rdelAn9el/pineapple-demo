9e8e14d3682eda5be91a11d0bed3dea42fdaff7f
Hector del Angel <hectordelangel@gmail.com>
2023-12-18 22:52:48 -0800

    Add maintenance mode redirect in ClientController and define maintenance_mode in app.php

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <div class="flex flex-col items-center justify-center h-screen">
+        <div class="text-3xl font-bold text-gray-700">503</div>
+        <div class="text-xl font-bold text-gray-700">Sorry, this page is under maintenance.</div>
+    </div>
+</x-app-layout>
