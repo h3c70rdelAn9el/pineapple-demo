bce56958336027a28fa7419e09a79fe19f29bf6a
Hector del Angel <hectordelangel@gmail.com>
2024-01-10 17:42:11 -0800

    Refactor form display and add verification feature

--- resources/views/therapist/forms-edit.blade.php
+++ resources/views/therapist/forms-edit.blade.php
@@ -10,57 +10,47 @@
                     Str::contains($form->file_name, '.png') ||
                     Str::contains($form->file_name, '.jpeg') ||
                     Str::contains($form->file_name, '.JPEG'))
-                <img class="h-64 w-full rounded-md md:h-[300px] md:w-full"
-                    src="{{ $form->url() }}" alt="{{ $form->file_name }}" />
+                <img class="h-64 w-full rounded-md md:h-[300px] md:w-full" src="{{ $form->url() }}"
+                    alt="{{ $form->file_name }}" />
             @endif
 
-            @if($user->admin == 1)
-            <a class="text-xs button" href="{{ $form->url() }}" download>
-                Download
-            </a>
+            @if ($user->admin == 1)
+                <a class="text-xs button" href="{{ $form->url() }}" download>
+                    Download
+                </a>
             @endif
         </div>
     </div>
 
-    <div class="flex flex-row justify-between w-full">
-        <div class="flex flex-row w-full">
-            <div class="flex flex-col w-1/2">
-                <div class="flex flex-row">
-                    <p class="text-xs font-light">Verified:</p>
-                    <p class="pl-2 text-xs font-light">
-                        @if ($form->verified == 1)
-                            Yes
-                        @else
-                            No
-                        @endif
-                    </p>
-                </div>
-                <div class="flex flex-row">
-                    <p class="text-xs font-light">Date:</p>
-                    <p class="pl-2 text-xs font-light">{{ $form->date }}</p>
-                </div>
-                <div class="flex flex-col flex-wrap">
-                    <p class="text-xs font-light">Document Type:</p>
-                    <p class="ml-2 text-xs font-light">{{ $form->document_type }}License</p>
-                </div>
-            </div>
-
-            <div class="flex flex-col w-1/2">
-                <p class="text-xs font-light">Notes:</p>
-                <p class="pl-2 text-xs font-light">{{ $form->notes }}</p>
-            </div>
+    <div class="flex flex-col w-1/2  mx-auto text-sm">
+        <div class="flex flex-row mt-2">
+            <p class="">Date:</p>
+            <p class="ml-4 ">{{ $form->date }}</p>
         </div>
-    </div>
-    @if ($user->admin == 1)
-        <form action="{{ route('fileUpdate', $form->id) }}" method="POST">
-            @csrf
-            @method('PUT')
-            <label for="verified">Verify</label>
-            <input type="hidden" name="verified" value="0">
-            <input id="verified" name="verified" type="checkbox" value="1">
-            <button type="submit">Submit</button>
+        <div class="flex flex-row flex-wrap mt-2">
+            <p class=" ">Document Type:</p>
+            <p class="  ml-4">{{ $form->document_type }}License</p>
+        </div>
+        <div class="flex flex-col w-full mt-2">
+            <p class=" ">Notes:</p>
+            <p class="border border-gray-200 ">{{ $form->notes }}
+            </p>
+        </div>
+        @if ($user->admin == 1)
+            @if ($form->verified == 1)
+                <p class=" mt-3 pb-2">Document has been verified</p>
+            @else
+                <form action="{{ route('fileUpdate', $form->id) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <label for="verified">Verify</label>
+                    <input type="hidden" name="verified" value="0">
+                    <input id="verified" name="verified" type="checkbox" value="1">
+                    <button type="submit">Submit</button>
 
-        </form>
-    @else
-    @endif
+                </form>
+            @endif
+        @else
+        @endif
+    </div>
 </x-app-layout>
