9531c1b72231d8e14835c8ffd1a0b16e7e19c085
Hector del Angel <hectordelangel@gmail.com>
2024-01-20 20:37:42 -0800

    Refactor client index page layout and add dynamic client filtering

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,45 +1,103 @@
 <x-app-layout>
-    <div>
-        <h2 class="text-center text-2xl">Clients</h2>
-    </div>
-    <div class="flex flex-row justify-between px-4 md:flex-row">
+    <div x-data="{
+        showAllClients: true,
+        showInactiveClients: false,
+        showWaitlistClients: false
+    }">
         <div>
-            <div class="flex flex-row gap-2">
-                <p>All Clients:</p>
-                <p>{{ $clients->count() }}</p>
+            <h2 class="text-center text-2xl">Clients</h2>
+        </div>
+        <div class="container mx-auto flex flex-row justify-between px-4 md:w-2/3 md:flex-row">
+            <div class="w-full md:w-1/2">
+
+                <button x-on:click="showAllClients = true">
+                    <div class="flex flex-row gap-2">
+                        <p>All Clients:</p>
+                        <p>{{ $clients->count() }}</p>
+                    </div>
+                </button>
+                <div class="flex flex-row gap-2">
+
+                    <button x-on:click="showInactiveClients = true, showAllClients = false, showWaitlistClients = false">
+                        <div class="flex w-full flex-row gap-2 border border-purple-300">
+                            <p>Inactive Clients:</p>
+                            <p>{{ $inactiveClients->count() }}</p>
+                        </div>
+                    </button>
+                </div>
+                <button x-on:click="showWaitlistClients = true, showAllClients = false, showInactiveClients = false">
+                    <div class="flex flex-row gap-2">
+                        <p>Waitlisted</p>
+                        <p>{{ $waitlistClients->count() }}</p>
+                    </div>
+                </button>
             </div>
-            <div class="flex flex-row gap-2">
-                <p>Inactive Clients:</p>
-                <p>{{ $inactiveClients->count() }}</p>
-            </div>
-            <div class="flex flex-row gap-2">
-                <p>Waitlisted</p>
-                <p>{{ $waitlistClients->count() }}</p>
+            <div>
+                <button class="button-secondary mt-4">
+                    <a
+                        class="text-sm"
+                        href="{{ route('clients.create') }}"
+                    >
+                        Add Client
+                    </a>
+                </button>
             </div>
         </div>
-        <div>
-            <button class="button-secondary mt-4">
-                <a
-                    class="text-sm"
-                    href="{{ route('clients.create') }}"
+        <section
+            class="container mx-auto p-6"
+            x-show="showAllClients"
+        >
+            @foreach ($clients as $client)
+                <x-client-card
+                    :client="$client"
+                    :therapist="$therapist"
+                    :user="$user"
                 >
-                    Add Client
-                </a>
-            </button>
-        </div>
+                </x-client-card>
+            @endforeach
+            {{-- add links --}}
+            {{-- <div class="flex justify-center">
+                {{ $clients->links() }}
+                </div> --}}
+        </section>
+        <section
+            class="container mx-auto p-6"
+            x-show="showInactiveClients"
+        >
+            @foreach ($inactiveClients as $client)
+                <x-client-card
+                    :client="$client"
+                    :therapist="$therapist"
+                    :user="$user"
+                >
+                </x-client-card>
+            @endforeach
+            {{-- add links --}}
+            {{-- <div class="flex justify-center">
+                {{ $clients->links() }}
+                </div> --}}
+        </section>
+        <section
+            class="container mx-auto p-6"
+            x-show="showWaitlistClients"
+        >
+            @foreach ($waitlistClients as $client)
+                <x-client-card
+                    :client="$client"
+                    :therapist="$therapist"
+                    :user="$user"
+                >
+                </x-client-card>
+            @endforeach
+            {{-- add links --}}
+            {{-- <div class="flex justify-center">
+                        {{ $clients->links() }}
+                        </div> --}}
+        </section>
     </div>
-    <section class="container mx-auto p-6">
-        @foreach ($clients as $client)
-            <x-client-card
-                :client="$client"
-                :therapist="$therapist"
-                :user="$user"
-            >
-            </x-client-card>
-        @endforeach
-        {{-- add links --}}
-        {{-- <div class="flex justify-center">
-            {{ $clients->links() }}
-            </div> --}}
-    </section>
 </x-app-layout>
+
+<script
+    src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
+    defer
+></script>
