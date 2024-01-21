a8782201628349e94bafe62fd484ef613daa3967
Hector del Angel <hectordelangel@gmail.com>
2024-01-21 10:27:16 -0800

    Add attended and missed sessions count to client table

--- resources/views/components/client-table.blade.php
+++ resources/views/components/client-table.blade.php
@@ -1,9 +1,8 @@
-<!-- component -->
 <div class="flex flex-col">
     <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
         <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
             <div class="overflow-hidden">
-                <table class="min-w-full">
+                <table class="min-w-full table-fixed">
                     <thead class="border-b bg-white">
                         <tr>
                             <th scope="col" class="px-6 py-4 text-left text-sm font-medium text-gray-900">
@@ -18,19 +17,19 @@
                             <th scope="col" class="px-6 py-4 text-left text-sm font-medium text-gray-900">
                                 Email
                             </th>
-                            <th scope="col" class="px-6 py-4 text-left text-sm font-medium text-gray-900">
+                            <th scope="col" class="max-w-20 px-6 py-4 text-left text-sm font-medium text-gray-900">
                                 Max Sessions
                             </th>
-                            <th scope="col" class="px-6 py-4 text-left text-sm font-medium text-gray-900">
+                            <th scope="col" class="max-w-20 px-6 py-4 text-left text-sm font-medium text-gray-900">
                                 Attended Sessions
                             </th>
-                            <th scope="col" class="px-6 py-4 text-left text-sm font-medium text-gray-900">
+                            <th scope="col" class="max-w-20 px-6 py-4 text-left text-sm font-medium text-gray-900">
                                 Missed Sessions
                             </th>
-                            <th scope="col" class="px-6 py-4 text-left text-sm font-medium text-gray-900">
+                            <th scope="col" class="w-6 px-6 py-4 text-left text-sm font-medium text-gray-900">
                                 Status
                             </th>
-                            <th scope="col" class="px-6 py-4 text-left text-sm font-medium text-gray-900">
+                            <th scope="col" class="w-6 px-6 py-4 text-left text-sm font-medium text-gray-900">
                                 Waitlist
                             </th>
                             <th scope="col" class="px-6 py-4 text-left text-sm font-medium text-gray-900">
@@ -39,34 +38,36 @@
                         </tr>
                     </thead>
                     <tbody>
-                        {{-- add the foreach loop here --}}
                         @foreach ($clients as $client)
-                            {{-- <tr class="border-b bg-gray-100"> --}}
                             <tr
-                                class="{{ $client->status == 1 ? 'bg-orange-300' : ($client->waitlist == 1 ? 'bg-blue-300' : 'bg-green-300') }} border-b">
-                                <td class="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900">
+                                class="{{ $client->status == 1 ? 'bg-orange-300' : ($client->waitlist == 1 ? 'bg-blue-300' : 'bg-green-300') }} border-b-4">
+                                <td class="whitespace-nowrap px-4 py-4 text-sm font-medium text-gray-900">
                                     {{ $client->client_code }}
                                 </td>
-                                <td class="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
+                                <td class="whitespace-nowrap px-4 py-4 text-sm font-light text-gray-900">
                                     {{ $client && $client->user && $client->user->preferred_name ? $client->user->preferred_name : ($client && $client->user ? $client->user->name : '') }}
                                 </td>
-                                <td class="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
+                                <td class="whitespace-nowrap px-4 py-4 text-sm font-light text-gray-900">
                                     {{ $client->possible_support_needed }}
                                 </td>
-                                <td class="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
+                                <td class="whitespace-nowrap px-4 py-4 text-sm font-light text-gray-900">
                                     {{ $client->email }}
                                 </td>
-                                <td class="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
+                                <td class="max-w-20 whitespace-nowrap px-4 py-4 text-sm font-light text-gray-900">
                                     {{ $client->max_sessions }}
                                 </td>
-                                <td class="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
-                                    {{ $client->attended_sessions }}
+                                <td class="max-w-20 whitespace-nowrap px-4 py-4 text-sm font-light text-gray-900">
+                                    {{ $client->therapySessions->whereIn('attendance', 'attended')->count() }}
+                                </td>
+                                <td class="max-w-20 whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
+                                    @if ($client->therapy_sessions)
+                                        {{ $client->therapy_sessions->where('attendance', 'no-show')->count() }}
+                                    @else
+                                        0
+                                    @endif
                                 </td>
                                 <td class="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
-                                    {{ $client->missed_sessions }}
-                                </td>
-                                <td class="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
-                                    {{ $client->status == 1 ? 'Active' : 'Inactive' }}
+                                    {{ $client->status == 1 ? 'Inactive' : 'Active' }}
                                 </td>
                                 <td class="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
                                     {{ $client->waitlist == 1 ? 'Yes' : 'No' }}
@@ -79,9 +80,9 @@
                         @endforeach
                     </tbody>
                     <tfoot class="border-b bg-white">
-                            <td colspan="10" class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                {{ $clients->links() }}
-                            </td>
+                        <td colspan="10" class="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900">
+                            {{ $clients->links() }}
+                        </td>
                     </tfoot>
                 </table>
             </div>
