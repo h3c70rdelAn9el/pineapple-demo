8e2e24a1ecb2acfa0bd36f0eb4de3dc6d4352a46
Hector del Angel <hectordelangel@gmail.com>
2024-01-21 16:03:59 -0800

    Refactor client-card.blade.php to include special sessions background color

--- resources/views/components/client-card.blade.php
+++ resources/views/components/client-card.blade.php
@@ -1,17 +1,23 @@
 <a href="{{ route('clients.show', $client->id) }}"
-    class="{{ $client->status == 1 ? 'bg-orange-300' : ($client->waitlist == 1 ? 'bg-blue-300' : 'bg-green-300') }} min-h-content relative z-0 m-2 flex flex-col justify-between rounded-lg border border-blue-200 p-2 shadow-md shadow-blue-100 transition-all duration-200 ease-in hover:border hover:border-blue-500 hover:shadow-lg"
+    class="{{ $client->status == 1
+        ? 'bg-orange-300'
+        : ($client->waitlist == 1
+            ? 'bg-blue-300'
+            : ($client->special_sessions > 0
+                ? 'bg-purple-300'
+                : 'bg-green-300')) }} min-h-content relative z-0 m-2 flex flex-row justify-between rounded-lg border border-blue-200 p-2 shadow-md shadow-blue-100 transition-all duration-200 ease-in hover:border hover:border-blue-500 hover:shadow-lg"
     style="z-index: 1;">
-    <div class="-mt-1.5 flex w-full flex-row justify-between p-1">
+    <div class="-mt-1.5 flex flex-col justify-between p-1">
         <p class="capitalize">{{ $client->client_code }}</p>
-        <p class="text-right text-sm">{{ $client->pronouns }}</p>
+        {{-- <p class="text-right text-sm">{{ $client->pronouns }}</p> --}}
+        @if ($user->admin)
+            <div class="flex flex-row justify-between p-1">
+                <p class="text-left text-sm">Therapist:
+                    {{ $client && $client->user && $client->user->preferred_name ? $client->user->preferred_name : ($client && $client->user ? $client->user->name : '') }}
+                </p>
+            </div>
+        @endif
     </div>
-    @if ($user->admin)
-        <div class="flex flex-row justify-between p-1">
-            <p class="text-left text-sm">Therapist:
-                {{ $client && $client->user && $client->user->preferred_name ? $client->user->preferred_name : ($client && $client->user ? $client->user->name : '') }}
-            </p>
-        </div>
-    @endif
     <div class="-mb-1 p-1 text-xs">
         <p class="text-left text-xs">
             Sessions: {{ $client->therapySessions->whereIn('attendance', ['no-show', 'attended'])->count() }}
