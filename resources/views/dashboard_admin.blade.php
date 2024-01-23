b94fdf535077410fa19c7b6995c18c72eb862fa9
Hector del Angel <hectordelangel@gmail.com>
2024-01-23 10:15:36 -0800

    Update styling in dashboard_admin and therapist/index views

--- resources/views/dashboard_admin.blade.php
+++ resources/views/dashboard_admin.blade.php
@@ -36,17 +36,17 @@
                         <a href="{{ route('therapists.index') }}" class=""><button class="rounded-md border-2 border-blue-300 px-2 py-1 mb-1 transition duration-300 ease-in-out hover:bg-blue-400">Therapists:</button></a>
                         <p>{{ $therapists->count() }}</p>
                     </div>
-                    <div class="flex justify-between">
+                    <div class="flex justify-between text-blue-600">
                         <p>Active:</p>
-                        <p class='text-blue-600'>{{ $activeTherapists->count() }}</p>
+                        <p>{{ $activeTherapists->count() }}</p>
                     </div>
-                    <div class="flex justify-between">
+                    <div class="flex justify-between text-slate-500">
                         <p>Inactive:</p>
-                        <p class="text-orange-400">{{ $inactiveTherapists->count() }}</p>
+                        <p class="">{{ $inactiveTherapists->count() }}</p>
                     </div>
-                    <div class="flex justify-between">
+                    <div class="flex justify-between text-red-600">
                         <p>Incomplete Profiles:</p>
-                        <p class="text-red-600">{{ $incompleteTherapists->count() }}</p>
+                        <p >{{ $incompleteTherapists->count() }}</p>
                     </div>
                 </div>
             </x-slot>
