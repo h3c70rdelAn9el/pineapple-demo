eccbc25ca070e90288a38faf1870c7fc9be144d6
Hector del Angel <hectordelangel@gmail.com>
2024-01-29 19:27:51 -0800

    Refactor session table view and add boolean cast for special column in TherapySession model

--- resources/views/components/session-table.blade.php
+++ resources/views/components/session-table.blade.php
@@ -29,24 +29,10 @@
                     {{-- <tbody> --}}
 
                     @foreach ($therapySessions as $therapySession)
-                        <tr
-                            class="{{ $therapySession->attendance == 'attended'
-                                ? 'bg-orange-300'
-                                : ($therapySession->attendance == 'no-show'
-                                    ? 'bg-red-300'
-                                    : ($therapySession->special > 0
-                                        ? 'bg-purple-300'
-                                        : 'bg-green-300')) }} border-b-4">
-                            {{-- <td class="whitespace-nowrap px-4 py-4 text-sm font-medium text-gray-900">
-                                <a href="{{ route('clients.show', $client->id) }}"
-                                    class="text-blue-600 hover:text-blue-800">
 
-                                    {{ $therapySession->id }}
-                                </a>
-                            </td> --}}
+                        <tr class="{{ $therapySession->special == 1 ? 'bg-purple-300' : ($therapySession->attendance === 'no-show' ? 'bg-red-300' : 'bg-green-300') }} border-b-4">
                             <td class="whitespace-nowrap px-4 py-4 text-sm font-light text-gray-900">
-                                  {{ $therapySession->client->client_code }}
-
+                                {{ $therapySession->client->client_code }}
                             </td>
 
                             <td class="whitespace-nowrap px-4 py-4 text-sm font-light text-gray-900">
@@ -56,37 +42,15 @@
                                 {{ $therapySession->therapist->name }}
                             </td>
 
-
-                             <td class="max-w-20 whitespace-nowrap px-4 py-4 text-sm font-light text-gray-900">
-                                    {{ $therapySession->created_at ? $therapySession->created_at->format('m-d-Y') : 'N/A' }}
+                            <td class="max-w-20 whitespace-nowrap px-4 py-4 text-sm font-light text-gray-900">
+                                {{ $therapySession->created_at ? $therapySession->created_at->format('m-d-Y') : 'N/A' }}
                             </td>
-                             {{-- <td class="max-w-20 whitespace-nowrap px-4 py-4 text-sm font-light text-gray-900">
+                            {{-- <td class="max-w-20 whitespace-nowrap px-4 py-4 text-sm font-light text-gray-900">
                                 {{ $therapySession->notes }}
                             </td> --}}
                             <td class="max-w-20 whitespace-nowrap px-4 py-4 text-sm font-light text-gray-900">
                                 {{ $therapySession->special ? 'Yes' : 'No' }}
                             </td>
-
-                            {{-- <td class="max-w-20 whitespace-nowrap px-4 py-4 text-sm font-light text-gray-900">
-                                {{ $client->therapySessions->whereIn('attendance', 'attended')->count() }}
-                            </td>
-                            <td class="max-w-20 whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
-                                @if ($client->therapy_sessions)
-                                    {{ $client->therapy_sessions->where('attendance', 'no-show')->count() }}
-                                @else
-                                    0
-                                @endif
-                            </td>
-                            <td class="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
-                                {{ $client->status == 1 ? 'Inactive' : 'Active' }}
-                            </td>
-                            <td class="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
-                                {{ $client->waitlist == 1 ? 'Yes' : 'No' }}
-                            </td>
-                            <td class="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
-                                <a href="{{ route('clients.show', $client->id) }}"
-                                    class="text-indigo-600 hover:text-indigo-900">View</a>
-                            </td> --}}
                         </tr>
                     @endforeach
                     </tbody>
