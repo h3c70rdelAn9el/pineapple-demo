5c0e40e3e4ba71dedbe5ed721ae53aadf62b0358
Hector del Angel <hectordelangel@gmail.com>
2024-01-29 21:07:57 -0800

    Update CSS and table layout

--- resources/views/components/session-table.blade.php
+++ resources/views/components/session-table.blade.php
@@ -5,25 +5,25 @@
                 <table class="min-w-full table-fixed">
                     <thead class="border-b bg-white">
                         <tr>
-                            <th scope="col" class="px-4 py-4 text-left text-sm font-medium text-gray-900">
+                            <th scope="col" class="th-table-header">
                                 ID
                             </th>
-                            <th scope="col" class="px-4 py-4 text-left text-sm font-medium text-gray-900">
+                            <th scope="col" class="th-table-header">
                                 Client Code
                             </th>
-                            <th scope="col" class="px-4 py-4 text-left text-sm font-medium text-gray-900">
+                            <th scope="col" class="th-table-header">
                                 Attendance
                             </th>
-                            <th scope="col" class="px-4 py-4 text-left text-sm font-medium text-gray-900">
+                            <th scope="col" class="th-table-header">
                                 Therapist
                             </th>
-                            <th scope="col" class="max-w-20 text -gray-900 px-6 py-4 text-left text-sm font-medium">
+                            <th scope="col" class="max-w-20 th-table-header">
                                 Date
                             </th>
                             {{-- <th scope="col" class="px-6 py-4 text-left text-sm font-medium text-gray-900">
                                 Notes
                             </th> --}}
-                            <th scope="col" class="max-w-20 px-4 py-4 text-left text-sm font-medium text-gray-900">
+                            <th scope="col" class="max-w-20 th-table-header">
                                 Special Session
                             </th>
 
@@ -32,29 +32,29 @@
                     {{-- <tbody> --}}
 
                     @foreach ($therapySessions as $therapySession)
-
-                        <tr class="{{ $therapySession->special == 1 ? 'bg-purple-300' : ($therapySession->attendance === 'no-show' ? 'bg-red-300' : 'bg-green-300') }} border-b-4">
-                            <td class="whitespace-nowrap px-2 py-2 text-sm font-light text-gray-900">
+                        <tr
+                            class="{{ $therapySession->special == 1 ? 'bg-purple-300' : ($therapySession->attendance === 'no-show' ? 'bg-red-300' : 'bg-green-300') }} border-b-4">
+                            <td class="td-table-data">
                                 {{ $therapySession->client->id }}
                             </td>
-                            <td class="whitespace-nowrap px-2 py-2 text-sm font-light text-gray-900">
+                            <td class="td-table-data">
                                 {{ $therapySession->client->client_code }}
                             </td>
 
-                            <td class="whitespace-nowrap px-2 py-2 text-sm font-light text-gray-900">
+                            <td class="td-table-data">
                                 {{ $therapySession->attendance }}
                             </td>
-                            <td class="whitespace-nowrap px-2 py-2 text-sm font-light text-gray-900">
+                            <td class="td-table-data">
                                 {{ $therapySession->therapist->name }}
                             </td>
 
-                            <td class="max-w-20 whitespace-nowrap px-2 py-2 text-sm font-light text-gray-900">
+                            <td class="max-w-20 td-table-data">
                                 {{ $therapySession->created_at ? $therapySession->created_at->format('m-d-Y') : 'N/A' }}
                             </td>
                             {{-- <td class="max-w-20 whitespace-nowrap px-4 py-4 text-sm font-light text-gray-900">
                                 {{ $therapySession->notes }}
                             </td> --}}
-                            <td class="max-w-20 whitespace-nowrap px-4 py-2 text-sm font-light text-gray-900">
+                            <td class="max-w-20 td-table-data">
                                 {{ $therapySession->special ? 'Yes' : 'No' }}
                             </td>
                         </tr>
