f2e428fc651f44bf02f93db6c87407cdf7d4f4ff
Tanguy de Courson <tanguy@0x7a69.com>
2024-02-20 08:35:40 -0800

    Remove commented out code and fix indentation in clients/edit.blade.php

--- resources/views/clients/edit.blade.php
+++ resources/views/clients/edit.blade.php
@@ -587,13 +587,7 @@
                     @endphp
 
                     @foreach ($groupedTherapists as $state => $therapistsInState)
-                    {{--
-                        @php
-                            $activeTherapistsInState = $therapistsInState->filter(function ($therapist) {
-                                return $therapist->active_status == 0;
-                            });
-                        @endphp
-                                --}}
+                    
                         @if ($activeTherapistsInState->isNotEmpty())
                             <optgroup label="{{ $state }}">
                                 @foreach ($activeTherapistsInState as $therapist)
