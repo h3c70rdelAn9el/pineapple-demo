f0c7313642b0f293f2d8937a7b6a1b0b5a7a4542
Tanguy de Courson <tanguy@0x7a69.com>
2024-02-20 08:34:40 -0800

    Fix rendering issue in client edit view

--- resources/views/clients/edit.blade.php
+++ resources/views/clients/edit.blade.php
@@ -587,12 +587,13 @@
                     @endphp
 
                     @foreach ($groupedTherapists as $state => $therapistsInState)
+                    {{--
                         @php
                             $activeTherapistsInState = $therapistsInState->filter(function ($therapist) {
                                 return $therapist->active_status == 0;
                             });
                         @endphp
-
+                                --}}
                         @if ($activeTherapistsInState->isNotEmpty())
                             <optgroup label="{{ $state }}">
                                 @foreach ($activeTherapistsInState as $therapist)
