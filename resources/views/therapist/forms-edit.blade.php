1755808774e9140264709c7f85eff536fb2ea681
Hector del Angel <hectordelangel@gmail.com>
2024-03-10 11:03:42 -0700

    Update form-edit.blade.php with new fields and update button

--- resources/views/therapist/forms-edit.blade.php
+++ resources/views/therapist/forms-edit.blade.php
@@ -23,7 +23,7 @@
     </div>
 
     <div class="mx-auto flex w-2/3 flex-col text-sm md:w-1/2">
-        <div class="mt-2 flex flex-row">
+        {{-- <div class="mt-2 flex flex-row">
             <p class="">Date:</p>
             <p class="ml-4">{{ $form->date }}</p>
         </div>
@@ -40,7 +40,41 @@
             @else
                 <p class="rounded-md border border-gray-200 font-light">No notes available.</p>
             @endif
-        </div>
+        </div> --}}
+        <form action="{{ route('fileUpdate', $form->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="relative mb-5 mt-4">
+                <x-jet-label for="date" value="{{ __('Expiration Date') }}"
+                    x-bind:required="documentType == 'clinical_license' || documentType ==
+                        'public_liability_insurance' || documentType == 'photographic_id'" />
+                <div class="relative">
+                    <x-jet-input class="mt-1 block w-[100%]" id="date" name="date" type="date"
+                        x-bind:required="['clinical_license', 'public_liability_insurance', 'photographic_id'].includes(documentType)"
+                        :value="old('date')" placeholder="Date" />
+                </div>
+            </div>
+
+            <div class="relative my-5">
+                <x-jet-label for="file_title" value="File Title" />
+                <div class="relative">
+                    <x-jet-input class="mt-1 block w-[100%]" id="file_title" name="file_title" type="text"
+                        :value="old('file_title')" placeholder="{{ $form->file_title }}" />
+                </div>
+
+            </div>
+
+            <div class="relative">
+                <x-jet-label for="note" value="{{ __('Note (optional)') }}" />
+                <textarea
+                    class="block w-full appearance-none rounded border border-blue-300 bg-gray-100 px-3 py-2 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+                    id="note" name="note" cols="40" rows="2" placeholder="{{ $form->note }}"></textarea>
+            </div>
+
+            <x-jet-button class=" right-0 mb-3 mr-6 mt-3" type="submit">
+                Update
+            </x-jet-button>
+        </form>
         @if ($user->admin == 1)
             @if ($form->verified == 1)
                 <p class="mt-3 pb-2">Document has been verified</p>
