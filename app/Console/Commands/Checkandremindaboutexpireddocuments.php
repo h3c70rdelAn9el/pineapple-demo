265805ab85562159c02582e587094667cad3268b
Tanguy de Courson <tanguy@0x7a69.com>
2024-08-21 14:31:19 -0700

    Refactor expired document check and reminder functionality

--- app/Console/Commands/Checkandremindaboutexpireddocuments.php
+++ app/Console/Commands/Checkandremindaboutexpireddocuments.php
@@ -36,28 +36,29 @@ class Checkandremindaboutexpireddocuments extends Command
             if ($cl && $cl->date < now()) {
                 //$user->notify(new DocumentsExpired());
                 //print "User " . $user->name . " has an expired clinical license that expired on " . $cl->date . "\n";
-                $userswithexpireddocuments[$user] = 'clinical license, ';
+                $userswithexpireddocuments[$user->id] = 'clinical license, ';
             }
             $photographic_id = $user->fileUploads()->where('document_type', 'photographic_id')->orderby('date', 'desc')->first();
             if ($photographic_id && $photographic_id->date < now()) {
                 //$user->notify(new DocumentsExpired());
                 //print "User " . $user->name . " has an expired photographic id that expired on " . $photographic_id->date . "\n";
-                $userswithexpireddocuments[$user] = 'photographic id, ';
+                $userswithexpireddocuments[$user->id] = 'photographic id, ';
             }
             $w9 = $user->fileUploads()->where('document_type', 'W9')->orderby('date', 'desc')->first();
             if ($w9 && $w9->date < now()) {
                 //$user->notify(new DocumentsExpired());
                 //print "User " . $user->name . " has an expired W9 that expired on " . $w9->date . "\n";
-                $userswithexpireddocuments[$user] = 'W9, ';
+                $userswithexpireddocuments[$user->id] = 'W9, ';
             }
             $w8ben = $user->fileUploads()->where('document_type', 'W8BEN')->orderby('date', 'desc')->first();
             if ($w8ben && $w8ben->date < now()) {
                 //$user->notify(new DocumentsExpired());
                 //print "User " . $user->name . " has an expired W8BEN that expired on " . $w8ben->date . "\n";
-                $userswithexpireddocuments[$user] = 'W8BEN, ';
+                $userswithexpireddocuments[$user->id] = 'W8BEN, ';
             }
         }
-        foreach ($userswithexpireddocuments as $user => $documents) {
+        foreach ($userswithexpireddocuments as $userid => $documents) {
+            $user = User::find($userid);
             //$user->notify(new DocumentsExpired(rtrim($documents)));
             print("User " . $user->name . " has expired documents: " . rtrim($documents) . "\n");
         }
