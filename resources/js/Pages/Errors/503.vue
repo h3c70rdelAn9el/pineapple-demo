811c0e324fc448631f5f89d4549a673211d43575
Tanguy de Courson <tanguy@0x7a69.com>
2025-04-08 09:38:52 -0700

    so, some issue in me trying to update to vue casued this weird issue of me commiting these files empty

---
+++ resources/js/Pages/Errors/503.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>503 Service Unavailable</h1>
+    <p>The server is currently unavailable. Please try again later.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error503"
+};
+</script>
+
+<style scoped>
+/* Add styles specific to the 503 error page here */
+</style>
