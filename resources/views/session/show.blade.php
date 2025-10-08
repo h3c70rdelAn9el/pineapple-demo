60dfc8f8d2e98f7187b2c4cb4075782ca7b38661
Tanguy de Courson <tanguy@decourson.com>
2025-10-08 09:55:09 -0700

    Refactor session details view for improved readability; adjust class order and formatting

--- resources/views/session/show.blade.php
+++ resources/views/session/show.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <section
-        class="mx-auto mt-3 h-full w-5/6 rounded-lg border border-black bg-gray-100 p-2 text-gray-800 shadow-md shadow-blue-100 lg:w-1/2">
+        class="w-5/6 h-full p-2 mx-auto mt-3 text-gray-800 bg-gray-100 border border-black rounded-lg shadow-md shadow-blue-100 lg:w-1/2">
         <div>
             <p class="text-lg">Client: <span class="font-bold capitalize">
                     <a href="{{ route('clients.show', $therapySession->client_id) }}"
                         class="text-blue-500 hover:text-blue-800">
                         {{ $therapySession->client->preferred_name }}</a>
                 </span></p>
-            <p class="text-center text-lg">Session Details</p>
+            <p class="text-lg text-center">Session Details</p>
         </div>
         @foreach ([
         'Preferred Name' => $client->preferred_name,
@@ -20,7 +20,7 @@
         'Session Attendance' => $therapySession->attendance,
     ] as $label => $value)
             <div class="flex flex-row">
-                <div class="w-1/2 border-b border-r border-gray-400 p-2">{{ $label }}:</div>
+                <div class="w-1/2 p-2 border-b border-r border-gray-400">{{ $label }}:</div>
                 <div
                     class="@if ($label === 'Session Attendance') text-{{ $attendanceColor }} @endif w-1/2 border-b border-gray-400 p-2">
                     @if ($label === 'Session Attendance')
@@ -32,8 +32,8 @@
             </div>
         @endforeach
         <div class="flex flex-row">
-            <div class="w-1/2 border-r border-gray-400 p-2">Session Notes:</div>
-            <div class="w-1/2 border-gray-400 p-2">{{ $therapySession->notes }}</div>
+            <div class="w-1/2 p-2 border-r border-gray-400">Session Notes:</div>
+            <div class="w-1/2 p-2 border-gray-400">{{ $therapySession->notes }}</div>
         </div>
     </section>
 
@@ -42,7 +42,7 @@
     <form action="{{ route('session.destroy', $therapySession) }}" method="POST">
         @csrf
         @method('DELETE')
-        <button type="submit" class="btn btn-danger text-blue-500 hover:text-blue-800"
+        <button type="submit" class="text-blue-500 btn btn-danger hover:text-blue-800"
             onclick="return confirm('Are you really sure that you want to delete this session?')">Delete</button>
     </form>
     @endif
